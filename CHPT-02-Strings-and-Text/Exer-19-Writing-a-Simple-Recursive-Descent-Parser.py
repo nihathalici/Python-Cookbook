0adbc4823e4ea24cd1f6fb155fdebb4d2a5fd509
Nihat Halici <nihat.halici@gmail.com>
2022-05-20 08:51:24 +0200

    Add files via upload

---
+++ CHPT-02-Strings-and-Text/Exer-19-Writing-a-Simple-Recursive-Descent-Parser.py
@@ -0,0 +1,75 @@
+# Exer-19-Writing-a-Simple-Recursive-Descent-Parser.py
+
+import re
+import collections
+
+NUM = r'(?P<NUM>\d+)'
+PLUS = r'(?P<PLUS>\+)'
+MINUS = r'(?P<MINUS>\-)'
+TIMES = r'(?P<TIMES>\*)'
+DIVIDE = r'(?P<DIVIDE>/)'
+LPAREN = r'(?P<LPAREN>\()'
+RPAREN = r'(?P<RPAREN>\))'
+WS = r'(?P<WS>\s+)'
+
+master_pat = re.compile('|'.join([NUM, PLUS, MINUS, TIMES,
+                                  DIVIDE, LPAREN, RPAREN, WS]))
+Token = collections.namedtuple('Token', ['type', 'value'])
+
+def generate_tokens(text):
+    scanner = master_pat.scanner(text)
+    for m in iter(scanner.match, None):
+        tok = Token(m.lastgroup, m.group())
+        if tok.type != 'WS':
+            yield tok
+
+class ExpressionEvaluator:
+    '''
+    Implementation of a recursive descent parser. Each method
+    implements a single grammar rule. Use the ._accept() method
+    to test and accept the current lookahead token. Use the ._expect()
+    method to exactly match and discard the next token on on the input
+    (or raise a SyntaxError if it doesn't match).
+    '''
+
+    def parse(self, text):
+        self.tokens = generate_tokens(text)
+        self.tok = None
+        self.nexttok = None
+        self._advance()
+        return self.expr()
+
+    def _advance(self):
+        'Advance one token ahead'
+        self.tok, self.nexttok = self.nexttok, next(self.tokens, None)
+
+    def _accept(self, toktype):
+        'Test and consume the next token if it matches toktype'
+        if self.nexttok and self.nexttok.type == toktype:
+            self._advance()
+            return True
+        else:
+            return False
+
+    def _except(self, toktype):
+        pass
+
+    def expr(self):
+        pass
+
+    def term(self):
+        pass
+
+    def factor(self):
+        pass
+    
+
+    
+
+    
+        
+
+    
+        
+
+
