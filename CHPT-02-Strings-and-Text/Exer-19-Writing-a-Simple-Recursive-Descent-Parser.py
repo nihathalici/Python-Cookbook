26b9211cdf4db84be31bd5ab1b66a5c39b2c5af7
Nihat Halici <nihat.halici@gmail.com>
2022-05-21 09:36:30 +0200

    Update Exer-19-Writing-a-Simple-Recursive-Descent-Parser.py

--- CHPT-02-Strings-and-Text/Exer-19-Writing-a-Simple-Recursive-Descent-Parser.py
+++ CHPT-02-Strings-and-Text/Exer-19-Writing-a-Simple-Recursive-Descent-Parser.py
@@ -1,7 +1,9 @@
 # Exer-19-Writing-a-Simple-Recursive-Descent-Parser.py
 
+
 import re
 import collections
+
 
 NUM = r'(?P<NUM>\d+)'
 PLUS = r'(?P<PLUS>\+)'
@@ -52,24 +54,186 @@ class ExpressionEvaluator:
             return False
 
     def _except(self, toktype):
-        pass
+        'Consume next token if it matches toktype or raise SyntaxError'
+        if not self._accept(toktype):
+            raise SyntaxError('Expected ' + toktype) 
+        
 
     def expr(self):
-        pass
+        "expression ::= term { ('+'|'-') term}*"
+
+        exprval = self.term()
+        while self._accept('PLUS') or self._accept('MINUS'):
+            op = self.tok.type
+            right = self.term()
+            if op == 'PLUS':
+                exprval += right
+            elif op == 'MINUS':
+                exprval -= right
+        return exprval
 
     def term(self):
-        pass
+        "term ::= factor { ('*'|'/') factor }*"
+
+        termval = self.factor()
+        while self._accept('TIMES') or self._accept('DIVIDE'):
+            op = self.tok.type
+            right = self.factor()
+            if op == 'TIMES':
+                termval *= right
+            elif op == 'DIVIDE':
+                termval /= right
+        return termval
 
     def factor(self):
-        pass
+        "factor ::= NUM | ( expr )"
+
+        if self._accept('NUM'):
+            return int(self.tok.value)
+        elif self._accept('LPAREN'):
+            exprval = self.expr()
+            self._except('RPAREN')
+            return exprval
+        else:
+            raise SyntaxError('Expected NUMBER or LPAREN')
+    
+e = ExpressionEvaluator()
+
+#print( e.parse('2') )
+
+#print( e.parse('2 + 3') )
+
+#print( e.parse('2 + 3 * 4') )
+
+#print( e.parse('2 + (3 + 4) * 5') )
+
+#print( e.parse('2 + (3 + * 4)') ) # Error
+
+class ExpressionTreeBuilder(ExpressionEvaluator):
+    def expr(self):
+        "expression ::= term { ('+'|'-') term }"
+
+        exprval = self.term()
+        while self._accept('PLUS') or self._accept('MINUS'):
+            op = self.tok.type
+            right = self.term()
+            if op == 'PLUS':
+                exprval == ('+', exprval, right)
+            elif op == 'MINUS':
+                exprval = ('-', exprval, right)
+        return exprval
+
+    def term(self):
+        "term ::= factor { ('*'|'/') factor }"
+
+        termval = self.factor()
+        while self._accept('TIMES') or self._accept('DIVIDE'):
+            op = self.tok.type
+            right = self.factor()
+            if op == 'TIMES':
+                termval = ('*', termval, right)
+            elif op == 'DIVIDE':
+                termval = ('/', termval, right)
+        return termval
+    
+    def factor(self):
+        'factor ::= NUM | ( expr )'
+
+        if self._accept('NUM'):
+            return int(self.tok.value)
+        elif self._accept('LPAREN'):
+            exprval = self.expr()
+            self._expect('RPAREN')
+            return exprval
+        else:
+            raise SyntaxError('Expected NUMBER or LPAREN')
     
 
-    
+e = ExpressionTreeBuilder()
+#print(e.parse('2 + 3'))
+#print(e.parse('2 + 3 * 4'))
 
-    
-        
-
-    
-        
+###
 
 
+
+from re import T
+from ply.lex import lex
+from ply.yacc import yacc 
+
+tokens = [ 'NUM', 'PLUS', 'MINUS', 'TIMES', 'DIVIDE', 'LPAREN', 'RPAREN' ]
+
+t_ignore = ' \t\n'
+
+t_PLUS = r'\+'
+t_MINUS = r'-'
+t_TIMES = r'\*'
+t_DIVIDE = r'/'
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+
+def t_NUM(t):
+    r'\d+'
+    t.value = int(t.value)
+    return T
+
+def t_error(t):
+    print('Bad character: {!r}'.format(t.value[0]))
+    t.skip(1)
+
+lexer = lex()
+
+def p_expr(p):
+    '''
+    expr : expr PLUS term
+         | expr MINUS term
+    '''
+    if p[2] == '+':
+        p[0] = p[1] + p[3]
+    elif p[2] == '-':
+        p[0] = p[1] - p[3]
+
+def p_expr_term(p):
+    '''
+    expr : term
+    '''
+    p[0] = p[1]
+
+def p_term(p):
+    '''
+    term : term TIMES factor
+         | term DIVIDE factor
+    '''
+    if p[2] == '*':
+        p[0] = p[1] * p[3]
+    elif p[2] == '/':
+        p[0] = p[1] / p[3]
+
+def p_term_factor(p):
+    '''
+    term : factor
+    '''
+    p[0] = p[1]
+
+def p_factor(p):
+    '''
+    factor : NUM
+    '''
+    p[0] = p[1]
+
+def p_factor_group(p):
+    '''
+    factor : LPAREN expr RPAREN
+    '''
+    p[0] = p[2]
+
+def p_error(p):
+    print('Syntax error')
+
+parser = yacc()
+
+print( parser.parse('2') )
+
+print( parser.parse('2 + 3') )
+
+print( parser.parse('2+(3+4)*5') )
