58b91dec59be21d85d7de7c45a254e48f9a33ca8
Nihat Halici <nihat.halici@gmail.com>
2022-05-31 07:29:40 +0200

    Add files via upload

---
+++ CHPT-03-Numbers-Dates-and-Times/Exer-14-Finding-the-Date-Range-for-the-Current-Month.py
@@ -0,0 +1,27 @@
+# Exer-14-Finding-the-Date-Range-for-the-Current-Month.py
+
+from datetime import datetime, date, timedelta
+import calendar
+
+def get_month_range(start_date=None):
+    if start_date is None:
+        start_date = date.today().replace(day=1)
+    _, days_in_month = calendar.monthrange(start_date.year, start_date.month)
+    end_date = start_date + timedelta(days=days_in_month)
+    return (start_date, end_date)
+
+a_day = timedelta(days=1)
+first_day, last_day = get_month_range()
+while first_day < last_day:
+    print(first_day)
+    first_day += a_day
+
+def date_range(start, stop, step):
+    while start < stop:
+        yield start
+        start += step
+
+for d in date_range(datetime(2012, 9, 1), datetime(2012, 10, 1), timedelta(hours=6)):
+    print(d)
+
+
