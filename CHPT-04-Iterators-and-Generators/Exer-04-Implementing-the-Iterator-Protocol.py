ae3b344876b4cb78eba8eb7bd64c0eaf2fb02449
Nihat Halici <nihat.halici@gmail.com>
2022-06-03 04:54:40 +0200

    Add files via upload

---
+++ CHPT-04-Iterators-and-Generators/Exer-04-Implementing-the-Iterator-Protocol.py
@@ -0,0 +1,32 @@
+# Exer-04-Implementing-the-Iterator-Protocol
+
+class Node:
+    def __init___(self, value):
+        self._value = value
+        self._children = []
+
+    def __repr__(self):
+        return 'Node({!r})'.format(self._value)
+
+    def add_child(self, node):
+        self._children.append(node)
+
+    def __iter__(self):
+        return iter(self._children)
+
+    def depth_first(self):
+        yield self
+        for c in self:
+            yield from c.depth_first()
+
+if __name__ == '__main__':
+    root = Node(0)
+    child1 = Node(1)
+    child2 = Node(2)
+    root.add_child(child1)
+    root.add_child(child2)
+    child1.add_child(Node(3))
+    child1.add_child(Node(4))
+    child2.add_child(Node(5))
+
+    
