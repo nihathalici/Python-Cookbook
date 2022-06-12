cb8f585cad0940a5a4df29981d2283ac3b02efb1
Nihat Halici <nihat.halici@gmail.com>
2022-06-12 12:19:25 +0200

    Add files via upload

---
+++ CHPT-04-Iterators-and-Generators/Exer-16-Replacing-Infinite-while-Loops-with-an-Iterator.py
@@ -0,0 +1,21 @@
+# Exer-16-Replacing-Infinite-while-Loops-with-an-Iterator
+
+CHUNKSIZE = 8192
+
+def reader(s):
+    while True:
+        data = s.recv(CHUNKSIZE)
+        if data == b'':
+            break
+        process_data(data)
+
+
+def reader(s):
+    for chunk in iter(lambda: s.recv(CHUNKSIZE), b''):
+        process_data(data)
+
+import sys
+
+f = open('/etc/passwd')
+for chunk in iter(lambda: f.read(10), ''):
+    n = sys.stdout.write(chunk)
