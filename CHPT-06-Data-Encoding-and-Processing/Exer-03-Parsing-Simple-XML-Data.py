99d33949b5df3b25ded2be999613000a60ebcf30
Nihat Halici <nihat.halici@gmail.com>
2022-06-27 09:29:46 +0200

    Add files via upload

---
+++ CHPT-06-Data-Encoding-and-Processing/Exer-03-Parsing-Simple-XML-Data.py
@@ -0,0 +1,26 @@
+# Exer-03-Parsing-Simple-XML-Data
+
+from urllib.request import urlopen
+from xml.etree.ElementTree import parse
+
+u = urlopen('http://planet.python.org/rss20.xml')
+doc = parse(u)
+
+'''
+for item in doc.iterfind('channel/item'):
+    title = item.findtext('title')
+    date = item.findtext('pubDate')
+    link = item.findtext('link')
+
+    print(title)
+    print(date)
+    print(link)
+    print()
+'''
+print(doc)
+e = doc.find('channel/title')
+print(e)
+print(e.tag)
+print(e.text)
+
+import lxml
