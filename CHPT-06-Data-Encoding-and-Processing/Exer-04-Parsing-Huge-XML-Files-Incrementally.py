9f6b95d7fce4bfe54bebe5c0ea3a397954be6a7c
Nihat Halici <nihat.halici@gmail.com>
2022-06-28 13:16:19 +0200

    Add files via upload

---
+++ CHPT-06-Data-Encoding-and-Processing/Exer-04-Parsing-Huge-XML-Files-Incrementally.py
@@ -0,0 +1,58 @@
+# Exer-04-Parsing-Huge-XML-Files-Incrementally
+
+from xml.etree.ElementTree import iterparse
+
+def parse_and_remove(filename, path):
+    path_parts = path.split('/')
+    doc = iterparse(filename, ('start', 'end'))
+    next(doc)
+
+    tag_stack = []
+    elem_stack = []
+    for event, elem in doc:
+        if event == 'start':
+            tag_stack.append(elem.tag)
+            elem_stack.append(elem)
+        elif event == 'end':
+            if tag_stack == path_parts:
+                yield elem
+                elem_stack[-2].remove(elem)
+            try:
+                tag_stack.pop()
+                elem_stack.pop()
+            except IndexError:
+                pass
+
+from xml.etree.ElementTree import parse
+from collections import Counter
+
+potholes_by_zip = Counter()
+doc = parse('potholes.xml')
+for pothole in doc.iterfind('row/row'):
+    potholes_by_zip[pothole.findtext('zip')] += 1
+for zipcode, num in potholes_by_zip.most_common():
+    print(zipcode, num)
+
+###
+
+from collections import Counter
+
+potholes_by_zip = Counter()
+
+data = parse_and_remove('potholes.xml', 'row/row')
+for pothole in data:
+    potholes_by_zip[pothole.findtext('zip')] += 1
+
+for zipcode, num in potholes_by_zip.most_common():
+    print(zipcode, num)
+
+###
+
+data = iterparse('potholes.xml', ('start', 'end'))
+next(data)
+
+
+    
+
+
+    
