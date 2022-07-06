b9872aa8c2f74770904dd30252ea290a0c63dc55
Nihat Halici <nihat.halici@gmail.com>
2022-07-06 08:23:34 +0200

    Update Exer-12-Reading-Nested-and-Variable-Sized-Binary-Structures.py

--- CHPT-06-Data-Encoding-and-Processing/Exer-12-Reading-Nested-and-Variable-Sized-Binary-Structures.py
+++ CHPT-06-Data-Encoding-and-Processing/Exer-12-Reading-Nested-and-Variable-Sized-Binary-Structures.py
@@ -73,25 +73,107 @@ class Structure:
         self._buffer = memoryview(bytedata)
 
 
+class PolyHeader(Structure):
+    file_code = StructField('<i', 0)
+    min_x = StructField('<d', 4)
+    min_y = StructField('<d', 12)
+    max_x = StructField('<d', 20)
+    max_y = StructField('<d', 28)
+    num_polys = StructField('<i', 36)
 
+f = open('polys.bin', 'rb')
+phead = PolyHeader(f.read(40))
+phead.file_code == 0x1234
+phead.min_x
+phead.min_y
+phead.max_x
+phead.max_y
+phead.num_polys
 
+###
 
+class StructureMeta(type):
+    '''
+    Metaclass that automatically creates StructField descriptors
+    '''
+    def __init__(self, clsname, bases, clsdict):
+        fields = getattr(self, '_fields_', [])
+        byte_order = ''
+        offset = 0
+        for format, fieldname in fields:
+            if format.startswith(('<','>','!','@')):
+                byte_order = format[0]
+                format = format[1:]
+            format = byte_order + format
+            setattr(self, fieldname, StructField(format, offset))
+            offset += struct.calcsize(format)
+        setattr(self, 'struct_size', offset)
 
+class Structure(metaclass=StructureMeta):
+    def __init__(self, bytedata):
+        self._buffer = bytedata
 
+    @classmethod
+    def from_file(cls, f):
+        return cls(f.read(cls.struct_size))
 
+class PolyHeader(Structure):
+    _fields_ = [
+        ('<i', 'file_code'),
+        ('d', 'min_x'),
+        ('d', 'min_y'),
+        ('d', 'max_x'),
+        ('d', 'max_y'),
+        ('i', 'num_polys')
+    ]
 
+f = open('polys.bin', 'rb')
+phead = PolyHeader.from_file(f)
+phead.file_code == 0x1234
+phead.min_x
+phead.min_y
+phead.max_x
+phead.max_y
+phead.num_polys
 
+###
 
+class NestedStruct:
+    '''
+    Descriptor representing a nested structure
+    '''
+    def __init__(self, name, struct_type, offset):
+        self.name = name
+        self.struct_type = struct_type
+        self.offset = offset
+    def __get__(self, instance, cls):
+        if instance is None:
+            return self
+        else:
+            data = instance._buffer[self.offset:
+                                    self.offset+self.struct_type.struct_size]
+            result = self.struct_type(data)
+            setattr(instance, self.name, result)
+            return result
 
-
-    
-    
-
-            
-        
-
-
-
-
-
-
+class StructureMeta(type):
+    '''
+    Metaclass that automatically creates StructField descriptors
+    '''
+    def __init__(self, clsname, bases, clsdict):
+        fields = getattr(self, '_fields_', [])
+        byte_order = ''
+        offset = 0
+        for format, fieldname in fields:
+            if isinstance(format, StructureMeta):
+                setattr(self, fieldname,
+                        NestedStruct(fieldname, format, offset))
+                offset += format.struct_size
+            else:
+                if format.startswith(('<','>','!','@')):
+                    byte_order = format[0]
+                    format = format[1:]
+                format = byte_order + format
+                setattr(self, fieldname, StructField(format, offset))
+                offset += struct.calcsize(format)
+        setattr(self, 'struct_size', offset)
