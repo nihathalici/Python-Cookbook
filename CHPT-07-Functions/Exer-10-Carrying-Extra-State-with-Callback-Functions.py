8c18b5d50bbe63602f3b3e0966c19517a9795f37
Nihat Halici <nihat.halici@gmail.com>
2022-07-15 09:40:29 +0200

    Add files via upload

---
+++ CHPT-07-Functions/Exer-10-Carrying-Extra-State-with-Callback-Functions.py
@@ -0,0 +1,68 @@
+# Exer-10-Carrying-Extra-State-with-Callback-Functions
+
+def apply_async(func, args, *, callback):
+    result = func(*args)
+    callback(result)
+
+def print_result(result):
+    print('Got:', result)
+
+def add(x, y):
+    return x + y
+
+
+apply_async(add, (2, 3), callback=print_result)
+apply_async(add, ('hello', 'world'), callback=print_result)
+
+
+class ResultHandler:
+    def __init__(self):
+        self.sequence = 0
+    def handler(self, result):
+        self.sequence += 1
+        print('[{}] Got: {}'.format(self.sequence, result))
+
+r = ResultHandler()
+apply_async(add, (2, 3), callback=r.handler)
+apply_async(add, ('hello', 'world'), callback=r.handler)
+
+def make_handler():
+    sequence = 0
+    def handler(result):
+        nonlocal sequence
+        sequence += 1
+        print('[{}] Got: {}'.format(sequence, result))
+    return handler
+
+handler = make_handler()
+apply_async(add, (2, 3), callback=handler)
+apply_async(add, ('hello', 'world'), callback=handler)
+
+
+def make_handler():
+    sequence = 0
+    while True:
+        result = yield
+        sequence += 1
+        print('[{}] Got: {}'.format(sequence, result))
+
+handler = make_handler()
+next(handler)
+apply_async(add, (2, 3), callback=handler.send)
+apply_async(add, ('hello', 'world'), callback=handler.send)
+    
+class SequenceNo:
+    def __init__(self):
+        self.sequence = 0
+
+def handler(result, seq):
+    seq.sequence += 1
+    print('[{}] Got: {}'.format(seq.sequence, result))
+
+seq = SequenceNo()
+from functools import partial
+
+apply_async(add, (2, 3), callback=partial(handler, seq=seq))
+apply_async(add, ('hello', 'world'), callback=partial(handler, seq=seq))
+
+apply_async(add, (2, 3), callback=lambda r: handler(r, seq))
