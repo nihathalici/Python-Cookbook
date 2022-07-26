8f5576687af1fde8dc9996f981a0033e766cd695
Nihat Halici <nihat.halici@gmail.com>
2022-07-26 12:11:53 +0200

    Update Exer-08-Extending-a-Property-in-a-Subclass.py

--- CHPT-08-Classes-and-Objects/Exer-08-Extending-a-Property-in-a-Subclass.py
+++ CHPT-08-Classes-and-Objects/Exer-08-Extending-a-Property-in-a-Subclass.py
@@ -53,3 +53,63 @@ class SubPerson(Person):
     def name(self, value):
         print('Setting name to', value)
         super(SubPerson, SubPerson).name.__set__(self, value)
+
+###
+        
+class SubPerson(Person):
+    @property
+    def name(self):
+        print('Getting name')
+        return super().name
+
+s = SubPerson('Guido')
+
+###
+
+class SubPerson(Person):
+    @Person.getter
+    def name(self):
+        print('Getting name')
+        return super().name
+
+s = SubPerson('Guido')
+s.name
+s.name = 'Larry'
+s.name = 42
+
+###
+
+class String:
+    def __init__(self, name):
+        self.name = name
+
+    def __get__(self, instance, cls):
+        if instance is None:
+            return self
+        return instance.__dict__[self.name]
+
+    def __set__(self, instance, value):
+        if not isinstance(value, str):
+            raise TypeError('Expected a string')
+        instance.__dict__[self.name] = value
+
+class Person:
+    name = String('name')
+    def __init__(self, name):
+        self.name = name
+
+class SubPerson(Person):
+    @property
+    def name(self):
+        print('Getting name')
+        return super().name
+
+    @name.setter
+    def name(self, value):
+        print('Setting name to', value)
+        super(SubPerson, SubPerson).name.__set__(self, value)
+
+    @name.deleter
+    def name(self):
+        print('Deleting name')
+        super(SubPerson, SubPerson).name.__delete__(self)
