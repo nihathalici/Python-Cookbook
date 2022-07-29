2e40fa2d090a8b3d3da376b2daae2a2227cb4a30
Nihat Halici <nihat.halici@gmail.com>
2022-07-29 08:08:42 +0200

    Add files via upload

---
+++ CHPT-08-Classes-and-Objects/Exer-11-Simplifying-the-Initialization-of-Data-Structures.py
@@ -0,0 +1,84 @@
+# Exer-11-Simplifying-the-Initialization-of-Data-Structures
+"""
+class Structure:
+    _fields = []
+    def __init__(self, *args):
+        if len(args) != len(self._fields):
+            raise TypeError('Expected {} arguments'.format(len(self._fields)))
+
+        for name, value in zip(self._fields, args):
+            setattr(self, name, value)
+
+# Example class definitions
+if __name__ == '__main__':
+    class Stock(Structure):
+        _fields = ['name', 'shares', 'price']
+
+    class Point(Structure):
+        _fields = ['x', 'y']
+
+    class Circle(Structure):
+        _fields = ['radius']
+        def area(self):
+            return math.pi * self.radius ** 2
+
+s = Stock('ACME', 50, 91.1)
+p = Point(2, 3)
+c = Circle(4.5)
+# s2 = Stock('ACME', 50)  # TypeError: Expected 3 arguments
+
+class Structure:
+    _fields = []
+    def __init__(self, *args, **kwargs):
+        if len(args) > len(self._fields):
+            raise TypeError('Expected {} arguments'.format(len(self._fields)))
+
+        # Set all of the positional arguments
+        for name, value in zip(self._fields, args):
+            setattr(self, name, value)
+
+        # Set the remaining keyword arguments
+        for name in self._fields[len(args):]:
+            setattr(self, name, kwargs.pop(name))
+
+        # Check for any remaining unknown arguments
+        if kwargs:
+            raise TypeError('Invalid argument(s): {}'.format(','.join(kwargs)))
+
+# Example use
+if __name__ == '__main__':
+    class Stock(Structure):
+        _fields = ['name', 'shares', 'price']
+
+    s1 = Stock('ACME', 50, 91.1)
+    s2 = Stock('ACME', 50, price=91.1)
+    s3 = Stock('ACME', shares=50, price=91.1)
+    
+###
+"""
+class Structure:
+    # Class variable that specifies expected fields
+    _fields = []
+    def __init__(self, *args, **kwargs):
+        if len(args) != len(self._fields):
+            raise TypeError('Expected {} arguments'.format(len(self._fields)))
+
+        # Set the arguments
+        for name, value in zip(self._fields, args):
+            setattr(self, name, value)
+
+        # Set the additional arguments (if any)
+        extra_args = kwargs.keys() - self._fields
+        for name in extra_args:
+            setattr(self, name, kwargs.pop(name))
+        if kwargs:
+            raise TypeError('Duplicate values for {}'.format(','.join(kwargs)))
+
+
+# Example use
+if __name__ == '__main__':
+    class Stock(Structure):
+        _fields = ['name', 'shares', 'price']
+
+    s1 = Stock('ACME', 50, 91.1)
+    s2 = Stock('ACME', 50, 91.1, date='8/2/2012')
