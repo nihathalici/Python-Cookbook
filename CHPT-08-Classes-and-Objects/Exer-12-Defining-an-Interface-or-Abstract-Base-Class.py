c408e33caf2b737d93166406034df71476db60da
Nihat Halici <nihat.halici@gmail.com>
2022-08-01 09:07:45 +0200

    Update Exer-12-Defining-an-Interface-or-Abstract-Base-Class.py

--- CHPT-08-Classes-and-Objects/Exer-12-Defining-an-Interface-or-Abstract-Base-Class.py
+++ CHPT-08-Classes-and-Objects/Exer-12-Defining-an-Interface-or-Abstract-Base-Class.py
@@ -17,4 +17,66 @@ class SocketStream(IStream):
         ...
     def write(self, data):
         ...
-        
+
+###
+
+def serialize(obj, stream):
+    if not isinstance(stream, IStream):
+        raise TypeError('Expected an IStream')
+
+###
+
+import io
+
+IStream.register(io.IOBase)
+
+f = open('foo.txt')
+isinstance(f, IStream)
+
+###
+
+from abc import ABCMeta, abstractmethod
+
+class A(metaclass=ABCMeta):
+    @property
+    @abstractmethod
+    def name(self):
+        pass
+
+    @name.setter
+    @abstractmethod
+    def name(self, value):
+        pass
+
+    @classmethod
+    @abstractmethod
+    def method1(cls):
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def method2():
+        pass
+
+###
+
+import collections
+
+if isinstance(x, collections.Sequence):
+    ...
+
+if isinstance(x, collections.Iterable):
+    ...
+
+if isinstance(x, collections.Sized):
+    ...
+
+if isinstance(x, collections.Mapping):
+    ...
+
+from decimal import Decimal
+import numbers
+
+x = Decimal('3.4')
+isinstance(x, numbers.Real)
+    
