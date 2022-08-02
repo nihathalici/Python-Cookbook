62478cf435c989ad2194e17b4d175f86cacfe885
Nihat Halici <nihat.halici@gmail.com>
2022-08-02 09:06:21 +0200

    Add files via upload

---
+++ CHPT-08-Classes-and-Objects/Exer-13-Implementing-a-Data-Model-or-Type-System.py
@@ -0,0 +1,53 @@
+# Exer-13-Implementing-a-Data-Model-or-Type-System
+
+# Base class. Uses a descriptor to set a value
+class Descriptor:
+    def __init__(self, name=None, **opts):
+        self.name = name
+        for key, value in opts.items():
+            setattr(self, key, value)
+
+    def __set__(self, instance, value):
+        instance.__dict__[self.name] = value
+
+# Descriptor for enforcing types
+class Typed(Descriptor):
+    expected_type = type(None)
+
+    def __set__(self, instance, value):
+        if not isinstance(value, self.expected_type):
+            raise TypeError('expected ' + str(self.expected_type))
+        super().__set__(instance, value)
+
+# Descriptor for enforcing values
+class Unsigned(Descriptor):
+    def __set__(self, instance, value):
+        if value < 0:
+            raise ValueError('Expected >= 0')
+        super().__set__(instance, value)
+
+class MaxSized(Descriptor):
+    def __init__(self, name=None, **opts):
+        if 'size' not in opts:
+            raise TypeError('missing size option')
+        super().__init__(name, **opts)
+
+    def __set__(self, instance, value):
+        if len(value) >= self.size:
+            raise ValueError('size must be < ' + str(self.size))
+        super().__set__(instance, value)
+
+###
+
+class Integer(Typed):
+    expected_type = int
+
+class UnsignedInteger(Integer, Unsigned):
+    pass
+
+class Float(Typed):
+    expected_type = float 
+
+            
+        
+        
