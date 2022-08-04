d5dbcc7374edc69bc10f33c968aaab1ec2a11998
Nihat Halici <nihat.halici@gmail.com>
2022-08-04 18:23:06 +0200

    Update Exer-13-Implementing-a-Data-Model-or-Type-System.py

--- CHPT-08-Classes-and-Objects/Exer-13-Implementing-a-Data-Model-or-Type-System.py
+++ CHPT-08-Classes-and-Objects/Exer-13-Implementing-a-Data-Model-or-Type-System.py
@@ -100,4 +100,115 @@ class Stock:
         self.name = name
         self.shares = shares
         self.price = price
-    
+
+###
+        
+# A metaclass that applies checking
+class checkedmeta(type):
+    def __new__(cls, clsname, bases, methods):
+        # Attach attribute names to the descriptors
+        for key, value in methods.items():
+            if isinstance(value, Descriptor):
+                value.name = key
+        return type.__new__(cls, clsname, bases, methods)
+
+# Example
+class Stock(metaclass=checkedmeta):
+    name = SizedString(size=8)
+    shares = UnsignedInteger()
+    price = UnsignedFloat()
+    def __init__(self, name, shares, price):
+        self.name = name
+        self.shares = shares
+        self.price = price
+            
+###
+
+# Normal
+class Point:
+    x = Integer('x')
+    y = Integer('y')
+
+# Metaclass
+class Point(metaclass=checkedmeta):
+    x = Integer()
+    y = Integer()
+
+###
+
+# Base class. Uses a descriptor to set a value
+class Descriptor:
+    def __init__(self, name=None, **opts):
+        self.name = name
+        for key, value in opts.items():
+            setattr(self,key, value)
+
+    def __set__(self, instance, value):
+        instance.__dict__[self.name] = value
+
+# Decorator for applying type checking
+def Typed(expected_type, cls=None):
+    if cls is None:
+        return lambda cls: Typed(expected_type, cls)
+
+    super_set = cls.__set__
+    def __set__(self, instance, value):
+        if not isinstance(value, expected_type):
+            raise TypeError('expected ' + str(expected_type))
+        super_set(self, instance, value)
+    cls.__set__ = __set__
+    return cls
+        
+# Decorator for unsigned values
+def Unsigned(cls):
+    super_set = cls.__set__
+
+    def __set__(self, instance, value):
+        if value < 0:
+            raise ValueError('Expected >= 0')
+        super_set(self, instance, value)
+    cls.__set__ = __set__
+    return cls
+
+# Decorator for allowing sized values
+def MaxSized(cls):
+    super_init = cls.__init__
+    def __init__(self, name=None, **opts):
+        if 'size' not in opts:
+            raise TypeError('missing size option')
+        super_init(self, name, **opts)
+    cls.__init__ = __init__
+
+    super_set = cls.__set__
+    def __set__(self, instance, value):
+        if len(value) >= self.size:
+            raise ValueError('size must be < ' + str(self.size))
+        super_set(self, instance, value)
+    cls.__set__ = __set__
+    return cls
+
+# Specialized descriptors
+@Typed(int)
+class Integer(Descriptor):
+    pass
+
+@Unsigned
+class UnsignedInteger(Integer):
+    pass
+
+@Typed(float)
+class Float(Descriptor):
+    pass
+
+@Unsigned
+class UnsignedFloat(Float):
+    pass
+
+@Typed(str)
+class String(Descriptor):
+    pass
+
+@MaxSized
+class SizedString(String):
+    pass
+
