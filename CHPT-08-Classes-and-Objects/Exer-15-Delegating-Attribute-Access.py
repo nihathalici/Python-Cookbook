18b00fe4ba32f9f7923f8a9d24db982dfccd1560
Nihat Halici <nihat.halici@gmail.com>
2022-08-08 10:16:59 +0200

    Update Exer-15-Delegating-Attribute-Access.py

--- CHPT-08-Classes-and-Objects/Exer-15-Delegating-Attribute-Access.py
+++ CHPT-08-Classes-and-Objects/Exer-15-Delegating-Attribute-Access.py
@@ -51,6 +51,91 @@ class Proxy:
         else:
             print('delattr:', name)
             delattr(self._obj, name)
+
+
+class Spam:
+    def __init__(self, x):
+        self.x = x
+    def bar(self, y):
+        print('Spam.bar:', self.x, y)
+
+# Create an instance
+s = Spam(2)
+
+# Create a proxy around it
+p = Proxy(s)
+
+# Access the proxy
+print(p.x)
+p.bar(3)
+p.x = 37
     
-    
-        
+###
+
+class A:
+    def spam(self, x):
+        print('A.spam', x)
+
+    def foo(self):
+        print('A.foo')
+
+class B(A):
+    def spam(self, x):
+        print('B.spam')
+        super().spam(x)
+
+    def bar(self):
+        print('B.bar')
+
+class A:
+    def spam(self, x):
+        print('A.spam', x)
+
+    def foo(self):
+        print('A.foo')
+
+
+class B:
+    def __init__(self):
+        self._a = A()
+
+    def spam(self, x):
+        print('B.spam', x)
+        self._a.spam(x)
+
+    def bar(self):
+        print('B.bar')
+
+    def __getattr__(self, name):
+        return getattr(self._a, name)
+
+###
+
+class ListLike:
+    def __init__(self):
+        self._items = []
+    def __getattr__(self, name):
+        return getattr(self._items, name)
+
+a = ListLike()
+a.append(2)
+a.insert(0, 1)
+a.sort()
+len(a)  # TypeError: object of type 'ListLike' has no len()
+a[0]  # TypeError: 'ListLike' object does not support indexing
+
+class ListLike:
+    def __init__(self):
+        self._items = []
+    def __getattr__(self, name):
+        return getattr(self._items, name)
+
+    # Added special methods to support certain list operations
+    def __len__(self):
+        return len(self._items)
+    def __getitem__(self, index):
+        return self._items[index]
+    def __setitem__(self, index, value):
+        return self._items[index] = value
+    def __delitem__(self, index):
+        return self._items[index]
