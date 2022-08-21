56847b361d14e6c611918fc0db9d7754795807e9
Nihat Halici <nihat.halici@gmail.com>
2022-08-21 10:09:56 +0200

    Update Exer-18-Extending-Classes-with-Mixins.py

--- CHPT-08-Classes-and-Objects/Exer-18-Extending-Classes-with-Mixins.py
+++ CHPT-08-Classes-and-Objects/Exer-18-Extending-Classes-with-Mixins.py
@@ -71,3 +71,58 @@ d = StringOrderedDict()
 d['x'] = 23
 d[42] = 10   # TypeError: keys must be strings
 d['x'] = 42  # KeyError: 'x already set'
+
+###
+
+from xmlrpc.server import SimpleXMLRPCServer
+from socketserver import ThreadingMixIn
+
+class ThreadedXMLRPCServer(ThreadingMixIn, SimpleXMLRPCServer):
+    pass
+
+class RestrictKeysMixin:
+    def __init__(self, *args, _restrict_key_type, **kwargs):
+        self.__restrict_key_type = _restrict_key_type
+        super().__init__(*args, **kwargs)
+    
+    def __setitem__(self, key, value):
+        if not isinstance(key, self.__restrict_key_type):
+            raise TypeError('Keys must be ' + str(self.__restrict_key_type))
+        super().__setitem__(key, value)
+
+class RDict(RestrictKeysMixin, dict):
+    pass
+
+d = RDict(_restrict_key_type = str)
+e = RDict([('name', 'Dave'), ('n', 37)], _restrict_key_type=str)
+f = RDict(name='Dave', n=37, _restrict_key_type=str)
+print(f)
+f[42] = 10  # TypeError: Keys must be <class 'str'>
+
+###
+
+def LoggedMapping(cls):
+    cls_getitem = cls.__getitem__
+    cls_setitem = cls.__setitem__
+    cls_delitem = cls.__delitem__
+
+    def __getitem__(self, key):
+        print('Getting ' + str(key))
+        return cls_getitem(self, key)
+    
+    def __setitem__(self, key, value):
+        print('Setting {} = {!r}'.format(key, value)) 
+        return cls_setitem(self, key, value)
+    
+    def __delitem__(self, key):
+        print('Deleting ' + str(key)) 
+        return cls_delitem(self, key)
+    
+    cls.__getitem__ = __getitem__
+    cls.__setitem__ = __setitem__
+    cls.__delitem__ = __delitem__
+    return cls
+
+@LoggedMapping
+class LoggedDict(dict):
+    pass
