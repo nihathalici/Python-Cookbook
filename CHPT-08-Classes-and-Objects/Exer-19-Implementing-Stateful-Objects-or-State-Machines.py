e6d99a9a634e70dbbbdf9c3231f4a1e2aa91d43a
Nihat Halici <nihat.halici@gmail.com>
2022-08-25 07:48:43 +0200

    Update Exer-19-Implementing-Stateful-Objects-or-State-Machines.py

--- CHPT-08-Classes-and-Objects/Exer-19-Implementing-Stateful-Objects-or-State-Machines.py
+++ CHPT-08-Classes-and-Objects/Exer-19-Implementing-Stateful-Objects-or-State-Machines.py
@@ -104,4 +104,55 @@ print(c.read())
 print(c.write('hello'))
 print(c.close())
 print(c._state)
+
+###
+
+# Original implementation
+class State:
+    def __init__(self):
+        self.state = 'A'
+    def action(self, x):
+        if state == 'A':
+            # Action for A
+            ...
+            state = 'B'
+        elif state == 'B':
+            # Action for B
+            ...
+            state = 'C'
+        elif state == 'C':
+            # Action for C
+            ...
+            state = 'A'
+
+# Alternative implementation
+class State:
+    def __init__(self):
+        self.new_state(State_A)
+    
+    def new_state(self, state):
+        self.__class__ = state
+    
+    def action(self, x):
+        raise NotImplementedError()
+
+class State_A(State):
+    def action(self, x):
+        # Action for A
+        ...
+        self.new_state(State_B)
+
+class State_B(State):
+    def action(self, x):
+        # Action for B
+        ...
+        self.new_state(State_C)
+
+class State_C(State):
+    def action(self, x):
+        # Action for C
+        ...
+        self.new_state(State_A)
+
+
     
