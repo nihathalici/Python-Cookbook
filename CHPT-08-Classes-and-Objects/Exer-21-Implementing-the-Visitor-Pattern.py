ef1b318c6219139dd29b41da1a530aab4e606715
Nihat Halici <nihat.halici@gmail.com>
2022-08-27 12:28:57 +0200

    Add files via upload

---
+++ CHPT-08-Classes-and-Objects/Exer-21-Implementing-the-Visitor-Pattern.py
@@ -0,0 +1,40 @@
+# Exer-21-Implementing-the-Visitor-Pattern
+
+class Node:
+    pass
+
+class UnaryOperator(Node):
+    def __init__(self, operand):
+        self.operand = operand
+
+class BinaryOperator(Node):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+class Add(BinaryOperator):
+    pass
+
+class Sub(BinaryOperator):
+    pass
+
+class Mul(BinaryOperator):
+    pass
+
+class Div(BinaryOperator):
+    pass
+
+class Negate(UnaryOperator):
+    pass
+
+class Number(Node):
+    def __init__(self, value):
+        self.value = value
+
+
+# Representation of 1 + 2 * (3 - 4) / 5
+t1 = Sub(Number(3), Number(4))
+t2 = Mul(Number(2), t1)
+t3 = Div(t2, Number(5))
+t4 = Add(Number(1), t3)
+
