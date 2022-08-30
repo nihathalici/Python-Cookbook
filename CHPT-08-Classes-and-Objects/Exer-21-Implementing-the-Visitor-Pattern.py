1d2d74b941ffa19827cb8369e7f1d432ae2ba205
Nihat Halici <nihat.halici@gmail.com>
2022-08-30 13:33:29 +0200

    Update Exer-21-Implementing-the-Visitor-Pattern.py

--- CHPT-08-Classes-and-Objects/Exer-21-Implementing-the-Visitor-Pattern.py
+++ CHPT-08-Classes-and-Objects/Exer-21-Implementing-the-Visitor-Pattern.py
@@ -38,3 +38,73 @@ t2 = Mul(Number(2), t1)
 t3 = Div(t2, Number(5))
 t4 = Add(Number(1), t3)
 
+###
+
+class NodeVisitor:
+    def visit(self, node):
+        methname = 'visit_' + type(node).__name__
+        meth = getattr(self, methname, None)
+        if meth is None:
+            meth = self.generic_visit
+        return meth(node)
+    
+    def generic_visit(self, node):
+        raise RuntimeError('No {} method'.format('visit_' + type(node).__name__))
+
+class Evaluator(NodeVisitor):
+    def visit_Number(self, node):
+        return node.value
+    
+    def visit_Add(self, node):
+        return self.visit(node.left) + self.visit(node.right)
+    
+    def visit_Sub(self, node):
+        return self.visit(node.left) - self.visit(node.right)
+    
+    def visit_Mul(self, node):
+        return self.visit(node.left) * self.visit(node.right)
+    
+    def visit_Add(self, node):
+        return -node.operand
+
+e = Evaluator()
+e.visit(t4)
+
+###
+
+class StackCode(NodeVisitor):
+    def generate_code(self, node):
+        self.instructions = []
+        self.visit(node)
+        return self.instructions
+    
+    def visit_Number(self, node):
+        self.instructions.append(('PUSH', node.value))
+    
+    def binop(self, node, instruction):
+        self.visit(node.left)
+        self.visit(node.right)
+        self.instructions.append((instruction,))
+    
+    def visit_Add(self, node):
+        self.binop(node, 'ADD')
+    
+    def visit_Sub(self, node):
+        self.binop(node, 'SUB')
+    
+    def visit_Mul(self, node):
+        self.binop(node, 'MUL')
+    
+    def visit_Div(self, node):
+        self.binop(node, 'DIV')
+    
+    def unaryop(self, node, instruction):
+        self.visit(node.operand)
+        self.instructions.append((instruction,))
+    
+    def visit_Negate(self, node):
+        self.unaryop(node, 'NEG')
+
+s = StackCode()
+s.generate_code(t4)
+
