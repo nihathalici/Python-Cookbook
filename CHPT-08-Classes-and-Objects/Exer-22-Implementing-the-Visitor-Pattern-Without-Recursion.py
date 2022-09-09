caf573342e7e908e5068712a364dc6a44e092b8e
Nihat Halici <nihat.halici@gmail.com>
2022-09-09 13:25:20 +0200

    Update Exer-22-Implementing-the-Visitor-Pattern-Without-Recursion.py

--- CHPT-08-Classes-and-Objects/Exer-22-Implementing-the-Visitor-Pattern-Without-Recursion.py
+++ CHPT-08-Classes-and-Objects/Exer-22-Implementing-the-Visitor-Pattern-Without-Recursion.py
@@ -166,3 +166,45 @@ try:
         last_result = stack.pop()
 except StopIteration:
     stack.pop()
+
+###
+
+class Visit:
+    def __init__(self, node):
+        self.node = node
+
+class NodeVisitor:
+    def visit(self, node):
+        stack = [ Visit(node) ]
+        last_result = None
+        while stack:
+            try:
+                last = stack[-1]
+                if isinstance(last, types.GeneratorType):
+                    stack.append(last.send(last_result))
+                    last_result = None
+                elif isinstance(last, Visit):
+                    stack.append(self._visit(stack.pop().node))
+                else:
+                    last_result = stack.pop()
+            except StopIteration:
+                stack.pop()
+        return last_result
+    
+    def _visit(self, node):
+        methname = 'visit_' + type(node).__name__
+        meth = getattr(self, methname, None)
+        if meth is None:
+            meth = self.generic_visit
+        return meth(node)
+    
+    def generic_visit(self, node):
+        raise RuntimeError('No {} method'.format('visit_' + type(node).__name__))
+
+class Evaluator(NodeVisitor):
+    ...
+    def visit_Add(self, node):
+        yield (yield Visit(node.left)) + (yield Visit(node.right))
+    
+    def visit_Sub(self, node):
+        yield (yield Visit(node.left)) - (yield Visit(node.right))
