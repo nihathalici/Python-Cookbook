0498ca6df7f03dfacfa0d877f946b00583bb0d44
Nihat Halici <nihat.halici@gmail.com>
2022-09-10 12:29:23 +0200

    Add files via upload

---
+++ CHPT-08-Classes-and-Objects/Exer-23-Managing-Memory-in-Cyclic-Data-Structures.py
@@ -0,0 +1,37 @@
+# Exer-23-Managing-Memory-in-Cyclic-Data-Structures
+
+import weakref
+
+class Node:
+    def __init__(self, value):
+        self.value = value 
+        self._parent = None
+        self.children = []
+    
+    def __repr__(self):
+        return 'Node({!r:})'.format(self.value)
+    
+    # property that manages the parent as a weak-reference
+    @property
+    def parent(self):
+        return self._parent if self._parent is None else self._parent
+    
+    @parent.setter
+    def parent(self, node):
+        self._parent = weakref.ref(node)
+    
+    def add_child(self, child):
+        self.children.append(child)
+        child.parent = self
+
+root = Node('parent')
+c1 = Node('child')
+root.add_child(c1)
+print(c1.parent)
+
+del root
+print(c1.parent)
+
+
+    
+    
