52c31ed74a797d78b0a4624599bc82a348db2aa8
Nihat Halici <nihat.halici@gmail.com>
2022-09-24 09:33:06 +0200

    Add files via upload

---
+++ CHPT-09-Metaprogramming/Exer-06-Defining-a-Decorator-That-Takes-an-Optional-Argument.py
@@ -0,0 +1,26 @@
+# Exer-06-Defining-a-Decorator-That-Takes-an-Optional-Argument
+
+from functools import wraps, partial 
+import logging
+
+def logged(func=None, *, level=logging.DEBUG, name=None, message=None):
+    if func is None:
+        return partial(logged, level=level, name=name, message=message)
+    
+    logname = name if name else func.__module__
+    log = logging.getLogger(logname)
+    logmsg = message if message else func.__name__
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        log.log(level, logmsg)
+        return func(*args, **kwargs)
+    return wrapper
+
+@logged
+def add(x, y):
+    return x + y
+
+@logged(level=logging.CRITICAL, name='example')
+def spam():
+    print('Spam!')
