9c69eb073a72e554220168862004a0088452126d
Nihat Halici <nihat.halici@gmail.com>
2022-10-01 12:45:24 +0200

    Update Exer-09-Defining-Decorators-As-Classes.py

--- CHPT-09-Metaprogramming/Exer-09-Defining-Decorators-As-Classes.py
+++ CHPT-09-Metaprogramming/Exer-09-Defining-Decorators-As-Classes.py
@@ -36,3 +36,34 @@ print(s.bar(1))
 print(s.bar(2))
 print(s.bar(3))
 print(Spam.bar.ncalls)
+
+###
+
+s = Spam()
+s.bar(3)
+
+s = Spam()
+def grok(self, x):
+    pass
+
+grok.__get__(s, Spam)
+
+###
+
+import types
+from functools import wraps
+
+def profiled(func):
+    ncalls = 0
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        nonlocal ncalls
+        ncalls += 1
+        return func(*args, **kwargs)
+    wrapper.ncalls = lambda: ncalls
+    return wrapper
+
+# Example
+@profiled
+def add(x, y):
+    return x + y
