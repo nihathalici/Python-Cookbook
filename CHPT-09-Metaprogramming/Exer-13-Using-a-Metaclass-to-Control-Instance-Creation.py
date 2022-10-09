cd92da80c390d05dba2d77a282504932d2081f8e
Nihat Halici <nihat.halici@gmail.com>
2022-10-09 13:25:46 +0200

    Add files via upload

---
+++ CHPT-09-Metaprogramming/Exer-13-Using-a-Metaclass-to-Control-Instance-Creation.py
@@ -0,0 +1,85 @@
+# Exer-13-Using-a-Metaclass-to-Control-Instance-Creation
+
+class Spam:
+    def __init__(self, name):
+        self.name = name
+
+a = Spam('Guido')
+b = Spam('Diana')
+
+### 
+
+class NoInstances(type):
+    def __call__(self, *args, **kwargs):
+        raise TypeError("Can't instantiate directly")
+
+# Example
+class Spam(metaclass=NoInstances):
+    @staticmethod
+    def grok(x):
+        print('Spam.grok')
+
+print(Spam.grok(42))
+# s = Spam()  # TypeError: Can't instantiate directly
+
+###
+
+class Singleton(type):
+    def __init__(self, *args, **kwargs):
+        self.__instance = None
+        super().__init__(*args, **kwargs)
+    
+    def __call__(self, *args, **kwargs):
+        if self.__instance is None:
+            self.__instance = super().__call__(*args, **kwargs)
+            return self.__instance
+        else:
+            return self.__instance
+
+# Example
+class Spam(metaclass=Singleton):
+    def __init__(self):
+        print('Creating Spam')
+
+a = Spam()
+b = Spam()
+print(a is b)
+c = Spam()
+print(a is c)
+
+###
+
+import weakref
+
+class Cached(type):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__cache = weakref.WeakValueDictionary()
+    
+    def __call__(self, *args):
+        if args in self.__cache:
+            return self.__cache[args]
+        else:
+            obj = super().__call__(*args)
+            self.__cache[args] = obj
+            return obj
+
+# Example
+class Spam(metaclass=Cached):
+    def __init__(self, name):
+        print('Creating Spam({!r})'.format(name))
+        self.name = name
+
+a = Spam('Guido')
+b = Spam('Diana')
+c = Spam('Guido')
+print(a is b)
+print(a is c)
+
+
+
+
+
+
+
+
