b7b4e0f8fb7c8ca04a7e1231766580185b72f046
Nihat Halici <nihat.halici@gmail.com>
2022-10-10 03:54:56 +0200

    Update Exer-13-Using-a-Metaclass-to-Control-Instance-Creation.py

--- CHPT-09-Metaprogramming/Exer-13-Using-a-Metaclass-to-Control-Instance-Creation.py
+++ CHPT-09-Metaprogramming/Exer-13-Using-a-Metaclass-to-Control-Instance-Creation.py
@@ -76,10 +76,19 @@ c = Spam('Guido')
 print(a is b)
 print(a is c)
 
+###
 
+class _Spam:
+    def __init__(self):
+        print('Creating Spam')
 
+_spam_instance = None
+def Spam():
+    global _spam_instance
 
-
-
-
-
+    if _spam_instance is not None:
+        return _spam_instance
+    else:
+        _spam_instance = _Spam()
+        return _spam_instance
+    
