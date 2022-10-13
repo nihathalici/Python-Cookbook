11bd7d0959508ea0e448e9fdb9816d7c3935fd19
Nihat Halici <nihat.halici@gmail.com>
2022-10-13 08:15:52 +0200

    Add files via upload

---
+++ CHPT-09-Metaprogramming/Exer-14-Capturing-Class-Attribute-Definition-Order.py
@@ -0,0 +1,39 @@
+# Exer-14-Capturing-Class-Attribute-Definition-Order
+
+from collections import OrderedDict
+
+# A set of descriptors for various types
+class Typed:
+    _expected_type = type(None)
+    def __init__(self, name=None):
+        self._name = name
+    
+    def __set__(self, instance, value):
+        if not isinstance(value, self._expected_type):
+            raise TypeError('Expected ' + str(self._expected_type))
+        instance.__dict__[self._name] = value
+
+class Integer(Typed):
+    _expected_type = int
+
+class Float(Typed):
+    _expected_type = float
+
+class String(Typed):
+    _expected_type = str
+
+# Metaclass that uses an OrderedDict for class body
+class OrderedMeta(type):
+    def __new__(cls, clsname, bases, clsdict):
+        d = dict(clsdict)
+        order = []
+        for name, value in clsdict.items():
+            if isinstance(value, Typed):
+                value._name = name
+                order.append(name)
+        d['_order'] = order
+        return type.__new__(cls, clsname, bases, d)
+    
+    @classmethod
+    def __prepare__(cls, clsname, bases):
+        return OrderedDict()
