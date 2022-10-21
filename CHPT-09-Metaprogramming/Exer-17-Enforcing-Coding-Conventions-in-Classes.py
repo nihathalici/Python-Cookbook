90b75cd184a4a98c23d1a4006b20dd8ed571c0a5
Nihat Halici <nihat.halici@gmail.com>
2022-10-21 07:51:38 +0200

    Add files via upload

--- CHPT-09-Metaprogramming/Exer-17-Enforcing-Coding-Conventions-in-Classes.py
+++ CHPT-09-Metaprogramming/Exer-17-Enforcing-Coding-Conventions-in-Classes.py
@@ -1,1 +1,46 @@
 # Exer-17-Enforcing-Coding-Conventions-in-Classes
+
+class MyMeta(type):
+    def __new__(self, clsname, bases, clsdict):
+        # clsname is name of class being defined
+        # bases is tuple of base classes
+        # clsdict is class dictionary
+        return super().__new__(cls, clsname, bases, clsdict)
+
+class MyMeta(type):
+    def __init__(self, clsname, bases, clsdict):
+        super().__init__(clsname, bases, clsdict)
+        # clsname is name of class being defined
+        # bases is tuple of base classes
+        # clsdict is class dictionary
+
+class Root(metaclass=MyMeta):
+    pass
+
+class A(Root):
+    pass
+
+class B(Root):
+    pass
+
+###
+
+class NoMixedCaseMeta(type):
+    def __new__(cls, clsname, bases, clsdict):
+        for name in clsdict:
+            if name.lower() != name:
+                raise TypeError('Bad attribute name: ' + name)
+        return super().__new__(cls, clsname, bases, clsdict)
+
+class Root(metaclass=NoMixedCaseMeta):
+    pass
+
+class A(Root):
+    def foo_bar(self):  # Ok
+        pass
+
+class B(Root):
+    def fooBar(self):  # TypeError
+        pass
+
+
