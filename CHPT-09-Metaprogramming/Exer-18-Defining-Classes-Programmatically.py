c4a982205d7154aaf0c5861b234b83575bb9cfd3
Nihat Halici <nihat.halici@gmail.com>
2022-10-23 12:50:48 +0200

    Add files via upload

--- CHPT-09-Metaprogramming/Exer-18-Defining-Classes-Programmatically.py
+++ CHPT-09-Metaprogramming/Exer-18-Defining-Classes-Programmatically.py
@@ -1,1 +1,22 @@
 # Exer-18-Defining-Classes-Programmatically
+
+def __init__(self, name, shares, price):
+    self.name = name
+    self.shares = shares
+    self.price = price
+
+def cost(self):
+    return self.shares * self.price
+
+cls_dict = {
+    '__init__' : __init__,
+    'cost' : cost,
+}
+
+import types
+
+Stock = types.new_class('Stock', (), {}, lambda ns: ns.update(cls_dict))
+Stock.__module__ = __name__
+
+s = Stock('ACME', 50, 91.1)
+print(s.cost())
