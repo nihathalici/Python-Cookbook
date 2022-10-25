3dbdf454492d6cf6d8365d77ae5881716ff77284
Nihat Halici <nihat.halici@gmail.com>
2022-10-25 06:36:56 +0200

    Update Exer-19-Initializing-Class-Members-at-Definition-Time.py

--- CHPT-09-Metaprogramming/Exer-19-Initializing-Class-Members-at-Definition-Time.py
+++ CHPT-09-Metaprogramming/Exer-19-Initializing-Class-Members-at-Definition-Time.py
@@ -1,1 +1,29 @@
 # Exer-19-Initializing-Class-Members-at-Definition-Time
+
+import operator
+
+class StructTupleMeta(type):
+    def __init__(cls, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for n, name in enumerate(cls._fields):
+            setattr(cls, name, property(operator.itemgetter(n)))
+
+class StructTuple(tuple, metaclass=StructTupleMeta):
+    _fields = []
+    def __new__(cls, *args):
+        if len(args) != len(cls._fields):
+            raise ValueError('{} arguments required'.format(len(cls._fields)))
+        return super().__new__(cls, args)
+
+class Stock(StructTuple):
+    _fields = ['name', 'shares', 'price']
+
+class Point(StructTuple):
+    _fields = ['x', 'y']
+
+s = Stock('ACME', 50, 91.1)
+print(s)
+print(s[0])
+print(s.name)
+print(s.shares * s.price)
+# print(s.shares)  # AttributeError: can't set attribute
