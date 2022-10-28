85ac1641939712db9e9742bac0e606cec308d679
Nihat Halici <nihat.halici@gmail.com>
2022-10-28 16:27:47 +0200

    Update Exer-20-Implementing-Multiple-Dispatch-with-Function-Annotations.py

--- CHPT-09-Metaprogramming/Exer-20-Implementing-Multiple-Dispatch-with-Function-Annotations.py
+++ CHPT-09-Metaprogramming/Exer-20-Implementing-Multiple-Dispatch-with-Function-Annotations.py
@@ -47,7 +47,54 @@ class MultiMethod:
                 self._methods[tuple(types)] = meth
             types.append(parm.annotation)
         self._methods[tuple(types)] = meth
-        
+    
+    def __call__(self, *args):
+        '''
+        Call a method based on type signature of the arguments
+        '''
 
+        types = tuple(type(arg) for arg in args[1:])
+        meth = self._methods.get(types, None)
+        if meth:
+            return meth(*args)
+        else:
+            raise TypeError('No matching method for types {}'.format(types))
+    
+    def __get__(self, instance, cls):
+        '''
+        Descriptor method needed to make calls work in a class
+        '''
+        if instance is not None:
+            return types.MethodType(self, instance)
+        else:
+            return self
+    
 
+class MultiDict(dict):
+    '''
+    Special dictionary to build multimethods in a metaclass
+    '''
+    def __setitem__(self, key, value):
+        if key in self:
+            # If key already exists, it must be a multimethod or callable
+            current_value = self[key]
+            if isinstance(current_value, MultiMethod):
+                current_value.register(value)
+            else:
+                mvalue = MultiMethod(key)
+                mvalue.register(current_value)
+                mvalue.register(value)
+                super().__setitem__(key, mvalue)
+        else:
+            super().__setitem__(key, value)
 
+class MultipleMeta(type):
+    '''
+    Metaclass that allows multiple dispatch of methods
+    '''
+    def __new__(cls, clsname, bases, clsdict):
+        return type.__new__(cls, clsname, bases, dict(clsdict))
+    
+    @classmethod
+    def __prepare__(cls, clsname, bases):
+        return MultiDict()
