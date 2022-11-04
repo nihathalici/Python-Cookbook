c2b7ace7bf91e3864d601e1c2c8f2156b0fa7b0e
Nihat Halici <nihat.halici@gmail.com>
2022-11-04 05:56:57 +0100

    Update Exer-24-Parsing-and-Analyzing-Python-Source.py

--- CHPT-09-Metaprogramming/Exer-24-Parsing-and-Analyzing-Python-Source.py
+++ CHPT-09-Metaprogramming/Exer-24-Parsing-and-Analyzing-Python-Source.py
@@ -54,3 +54,58 @@ print('Deleted:', c.deleted)
 
 print(exec(compile(top, '<stdin>', 'exec')))
 
+###
+
+import ast
+import inspect
+
+class NameLower(ast.NodeVisitor):
+    def __init__(self, lowered_names):
+        self.lowered_names = lowered_names
+    
+    def visit_FunctionDef(self, node):
+        code = '__globals = globals()\n'
+        code += '\n'.join("{} = __globals['{0}']".format(name)
+                           for name in self.lowered_names)
+        code_ast = ast.parse(code, mode='exec')
+
+        node.body[:0] = code_ast.body
+
+        self.func = node
+    
+    def lower_names(*namelist):
+        def lower(func):
+            srclines = inspect.getsource(func).splitlines()
+            for n, line in enumerate(srclines):
+                if '@lower_names' in line:
+                    break
+            
+            src = '\n'.join(srclines[n+1:])
+
+            if src.startswith((' ', '\t')):
+                src = 'if 1:\n' + src
+            top = ast.parse(src, mode='exec')
+
+            cl = NameLower(namelist)
+            cl.visit(top)
+
+            temp = {}
+            exec(compile(top, '', 'exec'), temp, temp)
+
+            func.__code__ = temp[func.__name__].__code__
+            return func
+        return lower
+
+INCR = 1
+
+@lower_names('INCR')
+def countdown(n):
+    while n > 0:
+        n -= INCR
+
+def countdown(n):
+    __globals = globals()
+    INCR = __globals['INCR']
+    while n > 0:
+        n -= INCR
+        
