46b121325b869a27ac5e118f6e99b46b0ecc8ba8
Nihat Halici <nihat.halici@gmail.com>
2022-11-12 03:05:59 +0100

    Add files via upload

---
+++ CHPT-10-Modules-and-Packages/Exer-07-Making-a-Directory-or-Zip-File-Runnable-As-a-Main-Script.py
@@ -0,0 +1,22 @@
+# Exer-07-Making-a-Directory-or-Zip-File-Runnable-As-a-Main-Script
+
+"""
+myapplication/
+  spam.py
+  bar.py
+  grok.py
+  __main__.py
+"""
+
+"""
+bash % python3 myapplication
+"""
+"""
+bash % ls
+bash % zip -r myapp.zip *.py
+bash % python3 myapp.zip
+"""
+
+"""
+#!/usr/bin/env python3 /usr/local/bin/myapp.zip
+"""
