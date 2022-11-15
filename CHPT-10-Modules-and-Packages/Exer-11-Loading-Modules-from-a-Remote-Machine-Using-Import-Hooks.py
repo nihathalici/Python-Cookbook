f10d55ecb112d6f31c456b9d032a70fd46074204
Nihat Halici <nihat.halici@gmail.com>
2022-11-15 07:10:05 +0100

    Update Exer-11-Loading-Modules-from-a-Remote-Machine-Using-Import-Hooks.py

--- CHPT-10-Modules-and-Packages/Exer-11-Loading-Modules-from-a-Remote-Machine-Using-Import-Hooks.py
+++ CHPT-10-Modules-and-Packages/Exer-11-Loading-Modules-from-a-Remote-Machine-Using-Import-Hooks.py
@@ -35,3 +35,35 @@ bash % cd testcode
 bash % python3 -m http.server 15000
 Serving HTTP on 0.0.0.0 port 15000 
 """
+
+from urllib.request import urlopen
+u = urlopen('http://localhost:15000/fib.py')
+data = u.read().decode('utf-8')
+print(data)
+
+###
+
+# fib.py
+print("I'm fib")
+
+def fib(n):
+    if n < 2:
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
+
+###
+
+import imp
+import urllib.request
+import sys
+
+def load_module(url):
+    u = urllib.request.urlopen(url)
+    source = u.read().decode('utf-8')
+    mod = sys.modules.setdefault(url, imp.new_module(url))
+    code = compile(source, url, 'exec')
+    mod.__file__ = url
+    mod.__package__ = ''
+    exec(code, mod.__dict__)
+    return mod
