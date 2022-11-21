291f3d11c372a4c137d2d182f3fbc6f88eacaf59
Nihat Halici <nihat.halici@gmail.com>
2022-11-21 09:43:30 +0100

    Update Exer-11-Loading-Modules-from-a-Remote-Machine-Using-Import-Hooks.py

--- CHPT-10-Modules-and-Packages/Exer-11-Loading-Modules-from-a-Remote-Machine-Using-Import-Hooks.py
+++ CHPT-10-Modules-and-Packages/Exer-11-Loading-Modules-from-a-Remote-Machine-Using-Import-Hooks.py
@@ -154,5 +154,75 @@ class UrlMetaFinder(importlib.abc.MetaPathFinder):
         log.debug('invalidating link cache')
         self._links.clear()
 
+# Module Loader for a URL
+class UrlModuleLoader(importlib.abc.SourceLoader):
 
+    def __init__(self, baseurl):
+        self._baseurl = baseurl
+        self._source_cache = {}
+    
+    def module_repr(self, module):
+        return '<urlmodule %r from %r>' % (module.__name__, module.__file__)
+    
+    # Required method
+    def load_module(self, fullname):
+        code = self.get_code(fullname)
+        mod = sys.modules.setdefault(fullname, imp.new_module(fullname))
+        mod.__file__ = self.get_filename(fullname)
+        mod.__loader__ = self
+        mod.__package__ = fullname.rpartition('.')[0]
+        exec(code, mod.__dict__)
+        return mod
+    
+    # Optional extensions
+    def get_code(self, fullname):
+        src = self.get_source(fullname)
+        return compile(src, self.get_filename(fullname), 'exec')
+    
+    def get_data(self, path):
+        pass
 
+    def get_filename(self, fullname):
+        return self._baseurl + '/' + fullname.split('.')[-1] + '.py'
+    
+    def get_source(self, fullname):
+        filename = self.get_filename(fullname)
+        log.debug('loader: reading %r, filename')
+        if filename in self._source_cache:
+            log.debug('loader: cached %r', filename)
+            return self._source_cache[filename]
+        try:
+            u = urlopen(filename)
+            source = u.read().decode('utf-8')
+            log.debug('loader: %r loaded', filename)
+            self._source_cache[filename] = source
+            return source
+        except (HTTPError, URLError) as e:
+            log.debug('loader: %r failed. %s', filename, e)
+            raise ImportError("Can't load %s" % filename)
+    
+    def is_package(self, fullname):
+        return False
+
+###
+
+def add(self, value):
+    self.raw_counter[value] += 1
+    self.mean = self.compute_mean()
+    self.stddev = self.compute_stddev()
+
+def __init__(self, counter:Counter=None):
+    if counter:
+        self.raw_counter = counter
+        self.count = sum(self.raw_counter[k] for k in self.raw_counter)
+        self.sum = sum(self.raw_counter[k]*k for k in self.raw_counter)
+        self.sum2 = sum(self.raw_counter[k]*k**2 for k in self.raw_counter)
+        self.mean = self.sum/self.count
+        self.stddev = math.sqrt((self.sum2 - self.sum**2/self.count) / (self.count - 1))
+    else:
+        self.raw_counter = Counter()
+        self.count = 0
+        self.sum = 0
+        self.sum2 = 0
+        self.mean = None
+        self.stddev = None
