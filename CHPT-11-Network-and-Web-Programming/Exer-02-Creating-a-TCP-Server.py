e0f315bca373256d034561ccb4483dbd484ebde3
Nihat Halici <nihat.halici@gmail.com>
2022-12-12 21:57:13 +0100

    Update Exer-02-Creating-a-TCP-Server.py

--- CHPT-11-Network-and-Web-Programming/Exer-02-Creating-a-TCP-Server.py
+++ CHPT-11-Network-and-Web-Programming/Exer-02-Creating-a-TCP-Server.py
@@ -1,1 +1,42 @@
 # Exer-02-Creating-a-TCP-Server
+
+from socketserver import BaseRequestHandler, TCPServer
+
+class EchoHandler(BaseRequestHandler):
+    def handle(self):
+        print('Got connection from', self.client_address)
+        while True:
+            msg = self.request.recv(8192)
+            if not msg:
+                break
+            self.request.send(msg)
+
+if __name__ == '__main__':
+    serv = TCPServer(('', 20000), EchoHandler)
+    serv.serve_forever()
+
+###
+
+from socket import socket, AF_INET, SOCK_STREAM
+
+s = socket(AF_INET, SOCK_STREAM)
+s.connect(('localhost', 20000))
+print(s.send(b'Hello'))
+print(s.recv(8192))
+
+###
+
+from socketserver import StreamRequestHandler, TCPServer
+
+class EchoHandler(StreamRequestHandler):
+    def handle(self):
+        print('Got connection from', self.client_address)
+        # self.rfile is a file-like object for reading
+        for line in self.rfile:
+            # self.wfile is a file-like object for writing
+            self.wfile.write(line)
+
+if __name__ == '__main__':
+    serv = TCPServer(('', 20000), EchoHandler)
+    serv.serve_forever()
+
