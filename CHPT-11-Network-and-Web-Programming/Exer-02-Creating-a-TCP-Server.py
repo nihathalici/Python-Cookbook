f598b1c1a7aa4d35e3dd92db86aee48124cb1328
Nihat Halici <nihat.halici@gmail.com>
2022-12-13 12:10:09 +0100

    Update Exer-02-Creating-a-TCP-Server.py

--- CHPT-11-Network-and-Web-Programming/Exer-02-Creating-a-TCP-Server.py
+++ CHPT-11-Network-and-Web-Programming/Exer-02-Creating-a-TCP-Server.py
@@ -40,3 +40,83 @@ if __name__ == '__main__':
     serv = TCPServer(('', 20000), EchoHandler)
     serv.serve_forever()
 
+###
+
+from socketserver import ThreadingTCPServer
+
+if __name__ == '__main__':
+    serv = ThreadingTCPServer(('', 20000), EchoHandler)
+    serv.serve_forever()
+
+###
+
+if __name__ == '__main__':
+    from threading import Thread
+    NWORKERS = 16
+    serv = TCPServer(('', 20000), EchoHandler)
+    for n in range(NWORKERS):
+        t = Thread(target=serv.serve_forever)
+        t.daemon = True
+        t.start()
+    serv.serve_forever()
+
+###
+
+if __name__ == '__main__':
+    serv = TCPServer(('', 20000), EchoHandler, bind_and_activate=False)
+    # Set up various socket options
+    serv.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
+    # Bind and activate
+    serv.server_bind()
+    serv.server_activate()
+    serv.serve_forever()
+
+###
+
+if __name__ == '__main__':
+    TCPServer.allow_reuse_address = True
+    serv = TCPServer(('', 20000), EchoHandler)
+    serv.serve_forever()
+
+###
+
+import socket
+
+class EchoHandler(StreamRequestHandler):
+    # Optional settings (defaults shown)
+    timeout = 5
+    rbufsize = -1
+    wbufsize = 0
+    disable_nagle_algorithm = False
+    def handle(self):
+        print('Got connection from', self.client_address)
+        try:
+            for line in self.rfile:
+                # self.wfile is a file-like object for writing
+                self.wfile.write(line)
+        except socket.timeout:
+            print('Timed out!')
+
+###
+
+from socket import socket, AF_INET, SOCK_STREAM
+
+def echo_handler(address, client_sock):
+    print('Got connection from {}'.format(address))
+    while True:
+        msg = client_sock.recv(8192)
+        if not msg:
+            break
+        client_sock.sendall(msg)
+    client_sock.close()
+
+def echo_server(address, backlog=5):
+    sock = socket(AF_INET, SOCK_STREAM)
+    sock.bind(address)
+    sock.listen(backlog)
+    while True:
+        client_sock, client_addr = sock.accept()
+        echo_handler(client_addr, client_sock)
+
+if __name__ == '__main__':
+    echo_server(('', 20000))
