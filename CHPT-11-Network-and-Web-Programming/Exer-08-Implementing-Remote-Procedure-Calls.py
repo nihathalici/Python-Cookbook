b2d59cac9a78b62824d56faeff059864df3367db
Nihat Halici <nihat.halici@gmail.com>
2022-12-22 07:03:34 +0100

    Update Exer-08-Implementing-Remote-Procedure-Calls.py

--- CHPT-11-Network-and-Web-Programming/Exer-08-Implementing-Remote-Procedure-Calls.py
+++ CHPT-11-Network-and-Web-Programming/Exer-08-Implementing-Remote-Procedure-Calls.py
@@ -1,2 +1,123 @@
 # Exer-08-Implementing-Remote-Procedure-Calls
 
+# rpcserver.py
+
+import pickle
+
+class RPCHandler:
+    def __init__(self):
+        self._functions = {}
+    
+    def register_function(self, func):
+        self._functions[func.__name__] = func
+    
+    def handle_connection(self, connection):
+        try:
+            while True:
+                # Receive a message
+                func_name, args, kwargs = pickle.loads(connection.recv())
+                # Run the RPC and send a response
+                try:
+                    r = self._functions[func_name](*args, **kwargs)
+                    connection.send(pickle.dumps(r))
+                except Exception as e:
+                    connection.send(pickle.dumps(e))
+        except EOFError:
+            pass
+
+###
+
+from multiprocessing.connection import Listener
+from threading import Thread
+
+def rpc_server(handler, address, authkey):
+    sock = Listener(address, authkey=authkey)
+    while True:
+        client = sock.accept()
+        t = Thread(target=handler.handle_connection, args=(client,))
+        t.daemon = True
+        t.start()
+
+# Some remote functions
+def add(x, y):
+    return x + y
+
+def sub(x, y):
+    return x - y
+
+# Register with a handler
+handler = RPCHandler()
+handler.register_function(add)
+handler.register_function(sub)
+
+# Run the server
+rpc_server(handler, ('localhost', 17000), authkey=b'peekaboo')
+
+###
+
+import pickle
+
+class RPCProxy:
+    def __init__(self, connection):
+        self._connection = connection
+    def __getattr__(self, name):
+        def do_rpc(*args, **kwargs):
+            self._connection.send(pickle.dumps((name, args, kwargs)))
+            result = pickle.loads(self._connection.recv())
+            if isinstance(result, Exception):
+                raise result
+            return result
+        return do_rpc
+
+###
+
+from multiprocessing.connection import Client
+
+c = Cient(('localhost', 17000), authkey=b'peekaboo')
+proxy = RPCProxy(c)
+proxy.add(2, 3)
+proxy.sub(2, 3)
+proxy.sub([1, 2], 4)
+
+###
+
+# jsonrpcserver.py
+
+import json
+
+class RPCHandler:
+    def __init__(self):
+        self._functions = {}
+    
+    def register_function(self, func):
+        self._functions[func.__name__] = func
+    
+    def handle_connection(self, connection):
+        try:
+            while True:
+                # Receive a message
+                func_name, args, kwargs = json.loads(connection.recv())
+                # Run the RPC and send a response
+                try:
+                    r = self._functions[func_name](*args, **kwargs)
+                    connection.send(json.dumps(r))
+                except Exception as e:
+                    connection.send(json.dumps(str(e)))
+        except EOFError:
+            pass
+
+ # jsonrpcclient.py
+
+import json
+
+class RPCProxy:
+    def __init__(self, connection):
+        self._connection = connection
+    def __getattr__(self, name):
+        def do_rpc(*args, **kwargs):
+            self._connection.send(json.dumps((name, args, kwargs)))
+            result = json.loads(self._connection.recv())
+            return result
+        return do_rpc
+
+
