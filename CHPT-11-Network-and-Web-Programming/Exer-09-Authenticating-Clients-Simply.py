8c90bd0a8b89b57259255c486d6c39388fc32b2f
Nihat Halici <nihat.halici@gmail.com>
2022-12-27 13:41:22 +0100

    Add files via upload

--- CHPT-11-Network-and-Web-Programming/Exer-09-Authenticating-Clients-Simply.py
+++ CHPT-11-Network-and-Web-Programming/Exer-09-Authenticating-Clients-Simply.py
@@ -1,1 +1,68 @@
 # Exer-09-Authenticating-Clients-Simply
+
+import hmac
+import os
+
+def client_authenticate(connection, secret_key):
+    '''
+    Authenticate client to a remote service.
+    connection represents a network connection. 
+    secret_key is a key known only to both client/server.
+    '''
+    message = connection.recv(32)
+    hash = hmac.new(secret_key, message)
+    digest = hash.digest()
+    connection.send(digest)
+
+def server_authenticate(connection, secret_key):
+    '''
+    Request client authentication.
+    '''
+    message = os.urandom(32)
+    connection.send(message)
+    hash = hmac.new(secret_key, message)
+    digest = hash.digest()
+    response = connection.recv(len(digest))
+    return hmac.compare_digest(digest, response)
+
+###
+
+from socket import socket, AF_INET, SOCK_STREAM
+
+secret_key = b'peekaboo'
+def echo_handler(client_sock):
+    if not server_authenticate(client_sock, secret_key):
+        client_sock.close()
+        return
+    while True:
+        msg = client_sock.recv(8192)
+        if not msg:
+            break
+        client_sock.sendall(msg)
+
+def echo_server(address):
+    s = socket(AF_INET, SOCK_STREAM)
+    s.bind(address)
+    s.listen(5)
+    while True:
+        c, a = s.accept()
+        echo_handler(c)
+
+echo_server(('', 18000))
+
+###
+
+from socket import socket, AF_INET, SOCK_STREAM
+
+secret_key = b'peekaboo'
+
+s = socket(AF_INET, SOCK_STREAM)
+s.connect(('localhost', 18000))
+client_authenticate(s, secret_key)
+s.send(b'Hello World')
+resp = s.recv(1024)
+
+
+
+
+
