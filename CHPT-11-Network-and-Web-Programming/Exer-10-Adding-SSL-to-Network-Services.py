83ab7e80b70a0c69b015b0eba261a86266b44749
Nihat Halici <nihat.halici@gmail.com>
2022-12-28 15:02:00 +0100

    Add files via upload

---
+++ CHPT-11-Network-and-Web-Programming/Exer-10-Adding-SSL-to-Network-Services.py
@@ -0,0 +1,37 @@
+# Exer-10-Adding-SSL-to-Network-Services
+
+from socket import socket, AF_INET, SOCK_STREAM
+import ssl
+
+KEYFILE = "server_key.pem"  # Private key of the server
+CERTFILE = "server_cert.pem"  # Server certificate (given to client)
+
+
+def echo_client(s):
+    while True:
+        data = s.recv(8192)
+        if data == b"":
+            break
+        s.send(data)
+    s.close()
+    print("Connection closed")
+
+
+def echo_server(address):
+    s = socket(AF_INET, SOCK_STREAM)
+    s.bind(address)
+    s.listen(1)
+
+    # Wrap with an SSL layer requiring client certs
+    s_ssl = ssl.wrap_socket(s, keyfile=KEYFILE, certfile=CERTFILE, server_side=True)
+    # Wait for connections
+    while True:
+        try:
+            c, a = s_ssl.accept()
+            print("Got connection", c, a)
+            echo_client(c)
+        except Exception as e:
+            print("{}: {}".format(e.__class__.__name__, e))
+
+
+echo_server(("", 20000))
