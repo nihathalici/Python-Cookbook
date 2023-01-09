e15667c5928711ee6ceea0abf3eb3e9310abef07
Nihat Halici <nihat.halici@gmail.com>
2023-01-09 20:30:38 +0100

    Update Exer-01-Starting-and-Stopping-Threads.py

--- CHPT-12-Concurrency/Exer-01-Starting-and-Stopping-Threads.py
+++ CHPT-12-Concurrency/Exer-01-Starting-and-Stopping-Threads.py
@@ -2,6 +2,7 @@
 
 # Code to execute in an independent thread
 import time
+import socket
 
 
 def countdown(n):
@@ -72,3 +73,30 @@ class IOTask:
                 continue
         # Terminated
         return
+
+
+###
+
+from threading import Thread
+
+
+class CountdownThread(Thread):
+    def __init__(self, n):
+        super().__init__()
+        self.n = 0
+
+    def run(self):
+        while self.n > 0:
+            print("T-minus", self.n)
+            self.n -= 1
+            time.sleep(5)
+
+
+c = CountdownThread(5)
+c.start()
+
+import multiprocessing
+
+c = CountdownTask(5)
+p = multiprocessing.Process(target=c.run)
+p.start()
