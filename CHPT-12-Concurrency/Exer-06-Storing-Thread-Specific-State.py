fd8cb552614b3b808f7fc9d7d3773a60297cf128
Nihat Halici <nihat.halici@gmail.com>
2023-01-19 06:58:39 +0100

    Add files via upload

---
+++ CHPT-12-Concurrency/Exer-06-Storing-Thread-Specific-State.py
@@ -0,0 +1,48 @@
+# Exer-06-Storing-Thread-Specific-State
+
+from socket import socket, AF_INET, SOCK_STREAM
+import threading
+
+
+class LazyConnection:
+    def __init__(self, address, family=AF_INET, type=SOCK_STREAM):
+        self.address = address
+        self.family = AF_INET
+        self.type = SOCK_STREAM
+        self.local = threading.local()
+
+    def __enter__(self):
+        if hasattr(self.local, "sock"):
+            raise RuntimeError("Already connected")
+        self.local.sock = socket(self.family, self.type)
+        self.local.sock.connect(self.address)
+        return self.local.sock
+
+    def __exit__(self, exc_ty, exc_val, tb):
+        self.local.sock.close()
+        del self.local.sock
+
+
+###
+
+from functools import partial
+
+
+def test(conn):
+    with conn as s:
+        s.send(b"GET /index.html HTTP/1.0\r\n")
+        s.send(b"Host: www.python.org\r\n")
+        s.send(b"\r\n")
+        resp = b"".join(iter(partial(s.recv, 8192), b""))
+    print("Got {} bytes".format(len(resp)))
+
+
+if __name__ == "__main__":
+    conn = LazyConnection(("www.python.org", 80))
+
+    t1 = threading.Thread(target=test, args=(conn,))
+    t2 = threading.Thread(target=test, args=(conn,))
+    t1.start()
+    t2.start()
+    t1.join()
+    t2.join()
