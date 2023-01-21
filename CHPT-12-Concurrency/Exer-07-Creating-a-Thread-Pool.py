14b06d009609eb2ad54dfa0361631ecbb0ffca09
Nihat Halici <nihat.halici@gmail.com>
2023-01-21 14:54:26 +0100

    Update Exer-07-Creating-a-Thread-Pool.py

--- CHPT-12-Concurrency/Exer-07-Creating-a-Thread-Pool.py
+++ CHPT-12-Concurrency/Exer-07-Creating-a-Thread-Pool.py
@@ -92,3 +92,43 @@ b = pool.submit(fetch_url, "http://www.pypy.org")
 # Get the results back
 x = a.result()
 y = b.result()
+
+###
+
+from threading import Thread
+from socket import socket, AF_INET, SOCK_STREAM
+
+
+def echo_client(sock, client_addr):
+    """
+    Handle a client connection
+    """
+    print("Got connection from", client_addr)
+    while True:
+        msg = sock.recv(65536)
+        if not msg:
+            break
+        sock.sendall(msg)
+    print("Client closed connection")
+    sock.close()
+
+
+def echo_server(addr, nworkers):
+    # Run the server
+    sock = socket(AF_INET, SOCK_STREAM)
+    sock.bind(addr)
+    sock.listen(5)
+    while True:
+        client_sock, client_addr = sock.accept()
+        t = Thread(target=echo_client, args=(client_sock, client_addr))
+        t.daemon = True
+        t.start()
+
+
+echo_server(("", 15000))
+
+###
+
+import threading
+
+threading.stack_size(65536)
