3de219569e78e8923593a7bdbe664645e2319dec
Nihat Halici <nihat.halici@gmail.com>
2023-01-30 07:02:14 +0100

    Update Exer-10-Defining-an-Actor-Task.py

--- CHPT-12-Concurrency/Exer-10-Defining-an-Actor-Task.py
+++ CHPT-12-Concurrency/Exer-10-Defining-an-Actor-Task.py
@@ -95,3 +95,63 @@ next(p)  # Advance to the yield (ready to receive)
 p.send("Hello")
 p.send("World")
 p.close()
+
+###
+
+
+class TaggedActor(Actor):
+    def run(self):
+        while True:
+            tag, *payload = self.recv()
+            getattr(self, "do_" + tag)(*payload)
+
+    # Methods correponding to different message tags
+    def do_A(self, x):
+        print("Running A", x)
+
+    def do_B(self, x, y):
+        print("Running B", x, y)
+
+
+# Example
+a = TaggedActor()
+a.start()
+a.send(("A", 1))  # Invokes do_A(1)
+a.send(("B", 2, 3))  # Invokes do_B(2, 3)
+
+###
+
+from threading import Event
+
+
+class Result:
+    def __init__(self):
+        self._evt = Event()
+        self._result = None
+
+    def set_result(self, value):
+        self._result = value
+        self._evt.set()
+
+    def result(self):
+        self._evt.wait()
+        return self._result
+
+
+class Worker(Actor):
+    def submit(self, func, *args, **kwargs):
+        r = Result()
+        self.send((func, args, kwargs, r))
+        return r
+
+    def run(self):
+        while True:
+            func, args, kwargs, r = self.recv()
+            r.set_result(func(*args, **kwargs))
+
+
+# Example use
+worker = Worker()
+worker.start()
+r = worker.submit(pow, 2, 3)
+print(r.result())
