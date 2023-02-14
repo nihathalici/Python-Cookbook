3c278e43e71e692795329ec4e2c86d02d157231c
Nihat Halici <nihat.halici@gmail.com>
2023-02-14 22:11:14 +0100

    Update Exer-12-Using-Generators-As-an-Alternative-to-Threads.py

--- CHPT-12-Concurrency/Exer-12-Using-Generators-As-an-Alternative-to-Threads.py
+++ CHPT-12-Concurrency/Exer-12-Using-Generators-As-an-Alternative-to-Threads.py
@@ -219,4 +219,52 @@ class WriteSocket(YieldEvent):
 
 
 class AcceptSocket(YieldEvent):
-    pass
+    def __init__(self, sock):
+        self.sock = sock
+
+    def handle_yield(self, sched, task):
+        sched._read_wait(self.sock.fileno(), self, task)
+
+    def handle_resume(self, sched, task):
+        r = self.sock.accept()
+        sched.add_ready(task, r)
+
+
+# Wrapper around a socket object for use with yield
+class Socket(object):
+    def __init__(self, sock):
+        self._sock = sock
+
+    def recv(self, maxbytes):
+        return ReadSocket(self._sock, maxbytes)
+
+    def send(self, data):
+        return WriteSocket(self._sock, data)
+
+    def accept(self):
+        return AcceptSocket(self._sock)
+
+    def __getattr__(self, name):
+        return getattr(self._sock, name)
+
+
+if __name__ == "__main__":
+    from socket import socket, AF_INET, SOCK_STREAM
+    import time
+
+    # Example of a function involving generators. This should
+    # be called using line = yield from readline(sock)
+    def readline(sock):
+        chars = []
+        while True:
+            c = yield sock.recv(1)
+            if not c:
+                break
+            chars.append(c)
+            if c == b"\n":
+                break
+        return b"".join(chars)
+
+    # Echo server using generators
+    class EchoServer:
+        pass
