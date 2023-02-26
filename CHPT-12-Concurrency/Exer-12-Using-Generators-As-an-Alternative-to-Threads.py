5ebd8087d01cf1f42825389d334849204bf03745
Nihat Halici <nihat.halici@gmail.com>
2023-02-26 23:43:13 +0100

    Update Exer-12-Using-Generators-As-an-Alternative-to-Threads.py

--- CHPT-12-Concurrency/Exer-12-Using-Generators-As-an-Alternative-to-Threads.py
+++ CHPT-12-Concurrency/Exer-12-Using-Generators-As-an-Alternative-to-Threads.py
@@ -296,3 +296,24 @@ if __name__ == "__main__":
 sched = Scheduler()
 EchoServer(("", 16000), sched)
 sched.run()
+
+###
+
+"""
+def some_generator():
+    ...
+    result = yield data
+
+
+f = some_generator()
+
+# Initial result. Is None to start since nothing has been computed
+result = None
+while True:
+    try:
+        data = f.send(result)
+        result = ... do some calculation ...
+    except StopIteration:
+        break
+
+"""
