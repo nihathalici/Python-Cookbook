06bae01db236cdfa07cdcdc36d0c1cb4b237eb49
Nihat Halici <nihat.halici@gmail.com>
2023-03-03 00:59:59 +0100

    Update Exer-13-Polling-Multiple-Thread-Queues.py

--- CHPT-12-Concurrency/Exer-13-Polling-Multiple-Thread-Queues.py
+++ CHPT-12-Concurrency/Exer-13-Polling-Multiple-Thread-Queues.py
@@ -30,3 +30,65 @@ class PollableQueue(queue.Queue):
         def get(self):
             self._getsocket.recv(1)
             return super().get()
+
+
+###
+
+import select
+import threading
+
+
+def consumer(queues):
+    """
+    Consumer that reads data on multiple queues simultaneously
+    """
+    while True:
+        can_read, _, _ = select.select(queues, [], [])
+        for r in can_read:
+            item = r.get()
+            print("Got:", item)
+
+
+q1 = PollableQueue()
+q2 = PollableQueue()
+q3 = PollableQueue()
+t = threading.Thread(target=consumer, args=([q1, q2, q3],))
+t.daemon = True
+t.start()
+
+# Feed data to the queues
+q1.put(1)
+q2.put(10)
+q3.put("hello")
+q2.put(15)
+
+###
+
+import time
+
+
+def consumer(queues):
+    while True:
+        for q in queues:
+            if not q.empty():
+                item = q.get()
+                print("Got:", item)
+    # Sleep briefly to avoid 100% CPU
+    time.sleep(0.01)
+
+
+###
+
+import select
+
+
+def event_loop(sockets, queues):
+    while True:
+        # polling with a timeout
+        can_read, _, _ = select.select(sockets, [], [], 0.01)
+        for r in can_read:
+            handle_read(r)
+        for q in queues:
+            if not q.empty():
+                item = q.get()
+                print("Got:", item)
