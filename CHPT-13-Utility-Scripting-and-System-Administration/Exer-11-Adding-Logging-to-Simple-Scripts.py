838c23c827091ad1e2ca0923107ef4aced1c8a0d
Nihat Halici <nihat.halici@gmail.com>
2023-03-31 18:47:44 +0200

    Add files via upload

---
+++ CHPT-13-Utility-Scripting-and-System-Administration/Exer-11-Adding-Logging-to-Simple-Scripts.py
@@ -0,0 +1,50 @@
+# Exer-11-Adding-Logging-to-Simple-Scripts
+
+import logging
+
+
+def main():
+    # Configure the logging system
+    logging.basicConfig(filename="app.log", level=logging.ERROR)
+
+    # Variables (to make the calls that follow work)
+    hostname = "www.python.org"
+    item = "spam"
+    filename = "data.csv"
+    mode = "r"
+
+    # Example logging calls (insert into your program)
+    logging.critical("Host %s unknown", hostname)
+    logging.error("Couldn't find %r", item)
+    logging.warning("Feature is deprecated")
+    logging.info("Opening file %r, mode=%r", filename, mode)
+    logging.debug("Got here")
+
+
+if __name__ == "__main__":
+    main()
+
+"""
+logging.basicConfig(filename='app.log', 
+                    level=logging.WARNING, 
+                    format="%(levelname)s:%(asctime)s:%(message)s")
+
+"""
+
+###
+
+"""
+import logging
+import logging.config
+
+def main():
+    # Configure the logging system
+    logging.config.fileConfig('logconfig.ini')
+    ...
+"""
+
+###
+
+# logging.basicConfig(level=logging.INFO)
+
+# logging.getLogger().level = logging.DEBUG
