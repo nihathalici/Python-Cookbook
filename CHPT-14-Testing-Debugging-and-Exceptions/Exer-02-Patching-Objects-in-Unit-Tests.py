a47051bd2da49f0196b705e70c30d21ad659e202
Nihat Halici <nihat.halici@gmail.com>
2023-04-05 01:57:34 +0200

    Update Exer-02-Patching-Objects-in-Unit-Tests.py

--- CHPT-14-Testing-Debugging-and-Exceptions/Exer-02-Patching-Objects-in-Unit-Tests.py
+++ CHPT-14-Testing-Debugging-and-Exceptions/Exer-02-Patching-Objects-in-Unit-Tests.py
@@ -41,3 +41,72 @@ def test2():
          patch('example.patch3') as mock3:
     ...
 """
+
+x = 42
+with patch("__main__.x"):
+    print(x)
+
+###
+
+with patch("__main__.x", "patched_value"):
+    print(x)
+
+###
+
+from unittest.mock import MagicMock
+
+m = MagicMock(return_value=10)
+m(1, 2, debug=True)
+m.assert_called_with(1, 2, debug=True)
+m.assert_called_with(1, 2)  # AssertionError
+
+m.upper.return_value = "HELLO"
+m.upper("hello")
+assert m.upper.called
+m.split.return_value = ["hello", "world"]
+m.split("hello world")
+m.split.assert_called_with("hello world")
+
+m["blah"]
+m.__getitem__.called
+m.__getitem__.assert_called_with("blah")
+
+###
+
+# example.py
+from urllib.request import urlopen
+import csv
+
+
+def dowprices():
+    u = urlopen("http://finance.yahoo.com/d/quotes.csv?s=@^DJI&f=sl1")
+    lines = (line.decode("utf-8") for line in u)
+    rows = (row for row in csv.reader(lines) if len(row) == 2)
+    prices = {name: float(price) for name, price in rows}
+    return prices
+
+
+import unittest
+from unittest.mock import patch
+import io
+import example
+
+simple_data = io.BytesIO(
+    b"""\
+"IBM",91.1\r
+"AA",13.25\r
+"MSFT",27.72\r
+\r
+"""
+)
+
+
+class Tests(unittest.TestCase):
+    @patch("example.urlopen", return_value=sample_data)
+    def test_dowprices(self, mock_urlopen):
+        p = example.dowprices()
+        self.assertTrue(mock_urlopen.called)
+        self.assertEqual(p, {"IBM": 91.1, "AA": 13.25, "MSFT": 27.72})
+
+    if __name__ == "__main__":
+        unittest.main()
