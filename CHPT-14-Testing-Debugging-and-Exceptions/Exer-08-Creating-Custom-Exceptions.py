0cc310e49b2dc1f86b9c631665bf35aef894017b
Nihat Halici <nihat.halici@gmail.com>
2023-04-10 12:34:06 +0200

    Add files via upload

---
+++ CHPT-14-Testing-Debugging-and-Exceptions/Exer-08-Creating-Custom-Exceptions.py
@@ -0,0 +1,67 @@
+# Exer-08-Creating-Custom-Exceptions
+
+
+class NetworkError(Exception):
+    pass
+
+
+class HostnameError(NetworkError):
+    pass
+
+
+class TimeoutError(NetworkError):
+    pass
+
+
+class ProtocolError(NetworkError):
+    pass
+
+
+###
+
+try:
+    msg = s.recv()
+except TimeoutError as e:
+    ...
+except ProtocolError as e:
+    ...
+
+###
+
+try:
+    s.send(msg)
+except ProtocolError:
+    ...
+
+###
+
+try:
+    s.send(msg)
+except NetworkError:
+    ...
+
+###
+
+
+class CustomError(Exception):
+    def __init__(self, message, status):
+        super().__init__(message, status)
+        self.message = message
+        self.status = status
+
+
+###
+
+try:
+    raise RuntimeError("It failed")
+except RuntimeError as e:
+    print(e.args)
+
+# 'It failed'
+
+try:
+    raise RuntimeError("It failed", 42, "spam")
+except RuntimeError as e:
+    print(e.args)
+
+# 'It failed', 42, 'spam'
