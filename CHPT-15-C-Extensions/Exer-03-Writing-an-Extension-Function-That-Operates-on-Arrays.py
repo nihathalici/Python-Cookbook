7199cea5879642a2b8dc524b90235d42c22e900f
Nihat Halici <nihat.halici@gmail.com>
2023-04-20 11:32:57 +0200

    Add files via upload

---
+++ CHPT-15-C-Extensions/Exer-03-Writing-an-Extension-Function-That-Operates-on-Arrays.py
@@ -0,0 +1,58 @@
+# Exer-03-Writing-an-Extension-Function-That-Operates-on-Arrays
+
+/* Call double avg(double *, int) */
+static PyObject *py_avg(PyObject *self, PyObject *args) {
+    PyObject *bufobj;
+    Py_buffer view;
+    double result;
+    /* Get the passed Python object */
+    if (!PyArg_ParseTuple(args, "0", &bufobj)) {
+    return NULL;
+    }
+}
+
+/* Attempt to extract buffer information from it */
+if (PyObject_GetBuffer(bufobj, &view,
+    PyBUF_ANY_CONTIGUOUS | PyBUF_FORMAT) == -1) {
+    return NULL; 
+}
+
+if (view.ndim != 1) {
+        PyErr_SetString(PyExc_TypeError, "Expected a 1-dimensional array");
+        PyBuffer_Release(&view);
+        return NULL;
+}
+
+/* Check the type of items in the array */
+if (strcmp(view.format, "d") != 0) {
+        PyErr_SetString(PyExc_TypeError, "Expected an array of doubles");
+        PyBuffer_Release(&view);
+        return NULL;
+}
+
+/* Pass the raw buffer and size to the C function */
+result = avg(view.buf, view.shape[0]);
+
+/* Indicate we're done working with the buffer */
+PyBuffer_Release(&view);
+return Py_BuildValue("d", result);
+
+###
+
+import array 
+
+avg(array.array('d', [1, 2, 3]))  # 2.0
+
+import numpy
+
+avg(numpy.array([1.0, 2.0, 3.0]))  # 2.0
+
+avg([1, 2, 3])  # TypeError
+
+avg(b'Hello')  # TypeError
+
+avg(a[:,2])  # ValueError
+
+sample.avg(a)  # TypeError
+
+sample.avg(a[0])  # 2.0
