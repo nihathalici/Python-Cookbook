22149e81ebb393a14b4e7e3226b05e36d3a5f16f
Nihat Halici <nihat.halici@gmail.com>
2023-04-25 09:33:08 +0200

    Update Exer-05-Defining-and-Exporting-C-APIs-from-Extension-Modules.py

--- CHPT-15-C-Extensions/Exer-05-Defining-and-Exporting-C-APIs-from-Extension-Modules.py
+++ CHPT-15-C-Extensions/Exer-05-Defining-and-Exporting-C-APIs-from-Extension-Modules.py
@@ -47,3 +47,50 @@ static int import_sample(void) {
 #ifdef __cplusplus
 }
 #endif
+
+###
+
+/* pysample.c */
+#include "Python.h"
+#define PYSAMPLE_MODULE
+#include "pysample.h"
+
+...
+/* Destructor function for points */
+static void del_Point(PyObject *obj) {
+    printf("Deleting point\n");
+    free(PyCapsule_GetPointer(obj, "Point"));
+}
+
+/* Utility functions */
+static Point *PyPoint_AsPoint(PyObject *obj) {
+    return (Point *) PyCapsule_GetPointer(obj, "Point");
+}
+
+static PyObject *PyPoint_FromPoint(Point *p, int free) {
+    return PyCapsule_New(p, "Point", free ? del_Point : NULL);
+}
+
+static _PointAPIMethods _point_api = {
+    PyPoint_AsPoint,
+    PyPoint_FromPoint
+};
+...
+
+/* Module initialization function */
+PyMODINIT_FUNC
+PyInit_sample(void) {
+    PyObject *m;
+    PyObject *py_point_api;
+
+    m = PyModule_Create(&samplemodule);
+    if (m == NULL)
+      return NULL;
+    
+    /* Add the Point C API functions */
+    py_point_api = PyCapsule_New((void *) &_point_api, "sample._point_api", NULL);
+    if (py_point_api) {
+      PyModule_AddObject(m, "_point_api", py_point_api);
+    }
+    return m; 
+}
