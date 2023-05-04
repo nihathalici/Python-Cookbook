71439c9764ee819ceec208b5a171d18d48f0027c
Nihat Halici <nihat.halici@gmail.com>
2023-05-04 04:18:55 +0200

    Add files via upload

---
+++ CHPT-15-C-Extensions/Exer-10-Wrapping-Existing-C-Code-with-Cython.py
@@ -0,0 +1,109 @@
+# Exer-10-Wrapping-Existing-C-Code-with-Cython
+
+# csample.pxd
+#
+# Declarations of "external" C functions and structures
+
+cdef extern from "sample.h":
+    int gcd(int, int)
+    int in_mandel(double, double, int n)
+    int divide(int, int, int *)
+    double avg(double *, int) nogil
+
+    ctypedef struct Point:
+        double x
+        double y
+
+    double distance(Point *, Point *)
+
+###
+
+# sample.pyx
+
+# Import the low-level C declarations
+cimport csample
+
+# Import some functionality from Python and the C stdlib
+from cpython.pycapsule cimport *
+from libc.stdlib cimport malloc, free 
+
+# Wrappers
+def gcd(unsigned int x, unsigned int y):
+    return csample.gcd(x, y)
+
+def in_mandel(x, y, unsigned int n):
+    return csample.in_mandel(x, y, n)
+
+def divide(x, y):
+    cdef int rem 
+    quot = csample.divide(x, y, &rem)
+    return quot, rem 
+
+def avg(double[:] a):
+    cdef:
+        int sz
+        double result  
+    
+    sz = a.size
+    with nogil:
+        result = csample.avg(<double *> &a[0], sz)
+    return result
+
+# Destructor for cleaning up Point objects
+cdef del_Point(object obj):
+    pt = <csample.Point *> PyCapsule_GetPointer(obj, "Point")
+    free(<void *> pt)
+
+# Create a Point object and return as a capsule
+def Point(double x, double y):
+    cdef csample.Point *p 
+    p = <csample.Point *> malloc(sizeof(csample.Point))
+    if p == NULL:
+        raise MemoryError("No memory to make a Point")
+    p.x = x
+    p.y = y 
+    return PyCapsule_New(<void *>p, "Point", <PyCapsule_Destructor>del_Point)
+
+def distance(p1, p2):
+    pt1 = <csample.Point *> PyCapsule_GetPointer(p1, "Point")
+    pt2 = <csample.Point *> PyCapsule_GetPointer(p2, "Point")
+    return csample.distance(pt1, pt2)
+
+###
+
+from distutils.core import setup 
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+
+ext_modules = [
+    Extension('sample',
+               ['sample.pyx'],
+               libraries=['sample'],
+               library_dirs=['.'])]
+
+setup(
+    name = 'Sample extension module',
+    cmdclass = {'build_ext': build_ext},
+    ext_modules = ext_modules
+)
+
+###
+
+# bash % python3 setup.py build_ext --inplace  # building sample.so
+
+###
+
+import sample
+sample.gcd(42, 10)  # 2
+sample.in_mandel(1, 1, 400)  # False
+sample.in_mandel(0, 0, 400)  # True
+sample.divide(42, 10)  # (4, 2)
+
+import array
+a = array.array("d", [1, 2, 3])
+sample.avg(a)  # 2.0
+
+p1 = sample.Point(2,3)
+p2 = sample.Point(4,5)
+p1  # <capsule object "Point" at...
+sample.distance(p1, p2)
