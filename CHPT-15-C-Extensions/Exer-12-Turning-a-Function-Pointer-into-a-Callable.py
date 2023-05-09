a713a3089ddd4d45f9fa80a77b87359bc3e7eab8
Nihat Halici <nihat.halici@gmail.com>
2023-05-09 09:51:54 +0200

    Update Exer-12-Turning-a-Function-Pointer-into-a-Callable.py

--- CHPT-15-C-Extensions/Exer-12-Turning-a-Function-Pointer-into-a-Callable.py
+++ CHPT-15-C-Extensions/Exer-12-Turning-a-Function-Pointer-into-a-Callable.py
@@ -14,3 +14,29 @@ func  # <CFunctionType object at ...
 # Call the resulting function
 func(2)  # 0.9092974268256817
 func(0)  # 0.0
+
+###
+
+from llvm.core import Module, Function, Type, Builder
+
+mod = Module.new("example")
+f = Function.new(
+    mod, Type.function(Type.double(), [Type.double(), Type.double()], False), "foo"
+)
+block = f.append_basic_block("entry")
+builder = Builder.new(block)
+x2 = builder.fmul(f.args[0], f.args[0])
+y2 = builder.fmul(f.args[1], f.args[1])
+r = builder.fadd(x2, y2)
+builder.ret(r)  # <llvm.core.Instruction object at...
+
+from llvm.ee import ExecutionEngine
+
+engine = ExecutionEngine.new(mod)
+ptr = engine.get_pointer_to_function(f)
+foo = ctypes.CFUNCTYPE(ctypes.c_double, ctypes.c_double, ctypes.c_double)(ptr)
+
+# Call the resulting function
+foo(2, 3)  # 13.0
+foo(4, 5)  # 41.0
+foo(1, 2)  # 5.0
